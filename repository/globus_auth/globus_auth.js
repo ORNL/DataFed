eb785920cd2e73556a4301dbd7d51e4f98b72892
Stansberry, Dale V <stansberrydv@ornl.gov>
2018-04-05 14:47:12 -0400

    - OAuth2 flow stuff

--- repository/globus_auth/globus_auth.js
+++ repository/globus_auth/globus_auth.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 var https = require('https');
 const fs = require('fs');
@@ -11,16 +13,52 @@ var privateKey  = fs.readFileSync( server_key, 'utf8');
 var certificate = fs.readFileSync( server_cert, 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
+const oauth_credentials = {
+    clientId: '7bc68d7b-4ad4-4991-8a49-ecbfcae1a454',
+    clientSecret: 'FpqvBscUorqgNLXKzlBAV0EQTdLXtBTTnGpf0+YnKEQ=',
+    authorizationUri: 'https://auth.globus.org/v2/oauth2/authorize',
+    accessTokenUri: 'https://auth.globus.org/v2/oauth2/token',
+    redirectUri: 'https://sdms.ornl.gov:7512/user_auth',
+    scopes: ['openid']
+};
+
+// Initialize the OAuth2 Library
+const ClientOAuth2 = require('client-oauth2');
+
+var globus_auth = new ClientOAuth2( oauth_credentials );
 
 app.get('/', (request, response) => {
-    response.send('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>SDMS Dev WebApp</title></head><body>SDMS Development WebApp</body></html>');
+    response.send('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>SDMS Dev WebApp</title></head><body>SDMS Development WebApp<br><br><a href="/go">Go!</a></body></html>');
+})
+
+app.get('/go', (request, response) => {
+    console.log(`starting auth flow` );
+    var uri = globus_auth.code.getUri();
+    response.redirect(uri)
 })
 
 app.get('/user_auth', (request, response) => {
     console.log(`user_auth: `, request.query, request.body );
-    response.status(200).end();
+
+    globus_auth.code.getToken( request.originalUrl ).then( function( user ) {
+        console.log( user ); //=> { accessToken: '...', tokenType: 'bearer', ... }
+
+        // Refresh the current users access token.
+        user.refresh().then( function (updatedUser) {
+            console.log( updatedUser !== user); //=> true
+            console.log( updatedUser.accessToken );
+        });
+
+        // Sign API requests on behalf of the current user.
+        user.sign({
+            method: 'get',
+            url: 'https://sdms.ornl.gov'
+        });
+
+        // We should store the token into a database.
+        return res.send( user.accessToken );
+    })
 })
 
 var httpsServer = https.createServer( credentials, app );
-
 httpsServer.listen( port );
