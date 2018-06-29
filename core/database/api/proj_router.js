231c1d2192b30727ae59e09b3220d156f2ca938e
Stansberry, Dale V <stansberrydv@ornl.gov>
2018-06-29 17:14:14 -0400

    - Fixes for project admin

--- core/database/api/proj_router.js
+++ core/database/api/proj_router.js
@@ -27,7 +27,7 @@ router.get('/create', function (req, res) {
         g_db._executeTransaction({
             collections: {
                 read: ["u","p"],
-                write: ["p","c","a","g","acl","owner","ident","alias","admin"]
+                write: ["p","c","a","g","acl","owner","ident","alias","admin","member"]
             },
             action: function() {
                 const client = g_lib.getUserFromClientID( req.queryParams.client );
@@ -44,6 +44,9 @@ router.get('/create', function (req, res) {
                         throw g_lib.ERR_NO_ALLOCATION;
                     proj_data.repo =  req.queryParams.repo;
                 }
+
+                if ( req.queryParams.desc )
+                    proj_data.desc = req.queryParams.desc;
 
                 var proj = g_db.p.save( proj_data, { returnNew: true });
                 g_db.owner.save({ _from: proj._id, _to: client._id });
@@ -65,16 +68,31 @@ router.get('/create', function (req, res) {
                 g_db.acl.save({ _from: root._id, _to: mem_grp._id, grant: g_lib.PERM_MEMBER, inhgrant: g_lib.PERM_MEMBER });
 
                 proj.new.admins = [];
+                proj.new.members = [];
                 proj.new.owner = client._key;
+                var uid;
 
                 if ( req.queryParams.admins ) {
                     for ( i in req.queryParams.admins ) {
-                        if ( req.queryParams.admins[i] == client._key )
+                        uid = req.queryParams.admins[i];
+                        if ( uid == client._key )
                             continue;
-                        if ( !g_db._exists( "u/" + req.queryParams.admins[i] ))
+                        if ( !g_db._exists( "u/" + uid ))
                             throw g_lib.ERR_USER_NOT_FOUND;
-                        g_db.admin.save({ _from: proj._id, _to: "u/" + req.queryParams.admins[i] });
-                        proj.new.admins.push( req.queryParams.admins[i] );
+                        g_db.admin.save({ _from: proj._id, _to: "u/" + uid });
+                        proj.new.admins.push( uid );
+                    }
+                }
+
+                if ( req.queryParams.members ) {
+                    for ( i in req.queryParams.members ) {
+                        uid = req.queryParams.members[i];
+                        if ( uid == client._key || proj.new.admins.indexOf( uid ) != -1 )
+                            continue;
+                        if ( !g_db._exists( "u/" + uid ))
+                            throw g_lib.ERR_USER_NOT_FOUND;
+                        g_db.member.save({ _from: mem_grp._id, _to: "u/" + uid });
+                        proj.new.members.push( uid );
                     }
                 }
 
@@ -96,8 +114,10 @@ router.get('/create', function (req, res) {
 .queryParam('id', joi.string().required(), "ID for new project")
 .queryParam('title', joi.string().required(), "Title (must be unque within domain)")
 .queryParam('domain', joi.string().required(), "Domain or topic (in reverse dotted notation)")
+.queryParam('desc', joi.string().required(), "Description")
 .queryParam('repo', joi.string().optional(), "Repository ID (must be associated with client)")
 .queryParam('admins', joi.array().items(joi.string()).optional(), "Additional project administrators (uids)")
+.queryParam('members', joi.array().items(joi.string()).optional(), "Project members (uids)")
 .summary('Create new project')
 .description('Create new project. For personal projects, domain should be user.uid (where \'uid\' is your user ID).');
 
@@ -109,11 +129,11 @@ router.get('/update', function (req, res) {
         g_db._executeTransaction({
             collections: {
                 read: ["u","p","uuid","accn"],
-                write: ["p","admin"]
+                write: ["p","admin","member"]
             },
             action: function() {
                 const client = g_lib.getUserFromClientID( req.queryParams.client );
-                var proj_id = "p/" + req.queryParams.id;
+                var proj_id = (req.queryParams.id.startsWith("p/")?"":"p/") + req.queryParams.id;
                 g_lib.ensureAdminPermProj( client, proj_id );
                 var owner_id = g_db.owner.firstExample({ _from: proj_id })._to.substr(2);
 
@@ -130,55 +150,54 @@ router.get('/update', function (req, res) {
                 if ( req.queryParams.title )
                     obj.title = req.queryParams.title;
 
-                if ( req.queryParams.repo ){
-                    if ( !g_lib.verifyRepo( client._id, req.queryParams.repo ))
-                        throw g_lib.ERR_NO_ALLOCATION;
-    
-                    obj.repo =  req.queryParams.repo;
-                }
-    
+                if ( req.queryParams.desc )
+                    obj.desc = req.queryParams.desc;
+
                 var proj = g_db._update( proj_id, obj, { keepNull: false, returnNew: true });
 
-                var admins = g_db._query( "for i in admin filter i._from == @proj return i._to", { proj: proj_id }).toArray();
-                for ( var i in admins ) {
-                    admins[i] = admins[i].substr( 2 );
-                }
+                var uid, i;
+                proj.new.admins = [];
+                proj.new.members = [];
 
-                var admin;
-
-                if ( req.queryParams.admin_remove ) {
-                    var idx;
-
-                    for ( i in req.queryParams.admin_remove ) {
-                        admin = req.queryParams.admin_remove[i];
-                        idx = admins.indexOf( admin );
-                        if ( idx != -1 ) {
-                            g_db.admin.removeByExample({ _from: proj_id, _to: "u/" + admin });
-                            admins.splice( idx, 1 );
-                        }
-                    }
-                }
-
-                if ( req.queryParams.admin_add ) {
-
-                    for ( i in req.queryParams.admin_add ) {
-                        admin = req.queryParams.admin_add[i];
-                        if ( admin == owner_id )
+                if ( req.queryParams.admins ) {
+                    g_db.admin.removeByExample({ _from: proj_id });
+                    for ( i in req.queryParams.admins ) {
+                        uid = req.queryParams.admins[i];
+                        if ( uid == owner_id )
                             continue;
-                        if ( admins.indexOf( admin ) == -1 ) {
-                            if ( !g_db._exists( "u/" + admin ))
-                                throw g_lib.ERR_USER_NOT_FOUND;
-
-                            g_db.admin.save({ _from: proj_id, _to: "u/" + admin });
-                            admins.push( admin );
-                        }
+                        if ( !g_db._exists( "u/" + uid ))
+                            throw g_lib.ERR_USER_NOT_FOUND;
+                        g_db.admin.save({ _from: proj._id, _to: "u/" + uid });
+                        proj.new.admins.push( uid );
                     }
+                }else{
+                    var admins = g_db._query( "for i in admin filter i._from == @proj return i._to", { proj: proj_id }).toArray();
+                    for ( i in admins ) {
+                        proj.new.admins.push( admins[i].substr( 2 ));
+                    }
+                }
+
+                if ( req.queryParams.members ) {
+                    var mem_grp = g_db.g.firstExample({ uid: proj_id, gid: "members" });
+                    g_db.member.removeByExample({ _from: mem_grp._id });
+                    for ( i in req.queryParams.members ) {
+                        uid = req.queryParams.members[i];
+                        if ( uid == owner_id || proj.new.admins.indexOf( uid ) != -1 )
+                            continue;
+                        if ( !g_db._exists( "u/" + uid ))
+                            throw g_lib.ERR_USER_NOT_FOUND;
+                        g_db.member.save({ _from: mem_grp._id, _to: "u/" + uid });
+                        proj.new.members.push( uid );
+                    }
+                }else{
+                    var members = g_db._query( "for v,e,p in 2..2 inbound @proj owner, outbound member filter p.vertices[1].gid == 'members' return v._key", { proj: proj_id }).toArray();
+
+                    if ( members.length )
+                        proj.new.members = members;
                 }
 
                 proj.new.id = proj.new._key;
                 proj.new.owner = owner_id;
-                proj.new.admins = admins;
-
 
                 delete proj.new._id;
                 delete proj.new._key;
@@ -197,9 +216,9 @@ router.get('/update', function (req, res) {
 .queryParam('id', joi.string().required(), "Project ID")
 .queryParam('title', joi.string().optional(), "New title")
 .queryParam('domain', joi.string().optional(), "Domain or topic (in reverse dotted notation)")
-.queryParam('repo', joi.string().optional(), "New repository ID (only affects new data)")
-.queryParam('admin_add', joi.array().items(joi.string()).optional(), "Account administrators (uids) to add")
-.queryParam('admin_remove', joi.array().items(joi.string()).optional(), "Account administrators (uids) to remove")
+.queryParam('desc', joi.string().optional(), "Description")
+.queryParam('admins', joi.array().items(joi.string()).optional(), "Account administrators (uids)")
+.queryParam('members', joi.array().items(joi.string()).optional(), "Project members (uids)")
 .summary('Update project information')
 .description('Update project information');
 
@@ -207,7 +226,7 @@ router.get('/update', function (req, res) {
 router.get('/view', function (req, res) {
     try {
         var client = g_lib.getUserFromClientID( req.queryParams.client );
-        var proj = g_db.p.document({ _id: "p/" + req.queryParams.id });
+        var proj = g_db.p.document({ _id: (req.queryParams.id.startsWith("p/")?"":"p/") + req.queryParams.id });
         var owner_id = g_db.owner.firstExample({_from: proj._id })._to.substr(2);
         var admins = g_db._query("for v in 1..1 outbound @proj admin return v._key", { proj: proj._id } ).toArray();
         if ( admins.length ) {
@@ -242,7 +261,7 @@ router.get('/view', function (req, res) {
 .description('View project information');
 
 router.get('/list/all', function (req, res) {
-    res.send( g_db._query( "for i in p return { uid: i._key, name: i.name }" ));
+    res.send( g_db._query( "for i in p return { id: i._key, name: i.name }" ));
 })
 .summary('List projects')
 .description('List projects');
@@ -259,17 +278,17 @@ router.get('/list', function (req, res) {
         qry = "";
 
     if ( !count || req.queryParams.by_owner ){
-        qry += "for v in 1..1 inbound @user owner filter IS_SAME_COLLECTION('p',v) return { uid: v._key, title: v.title }";
+        qry += "for v in 1..1 inbound @user owner filter IS_SAME_COLLECTION('p',v) return { id: v._key, title: v.title }";
         comma = (count != 1);
     }
 
     if ( !count || req.queryParams.by_admin ){
-        qry += (comma?"),(":"") + "for v in 1..1 inbound @user admin filter IS_SAME_COLLECTION('p',v) return { uid: v._key, title: v.title }";
+        qry += (comma?"),(":"") + "for v in 1..1 inbound @user admin filter IS_SAME_COLLECTION('p',v) return { id: v._key, title: v.title }";
         comma = (count != 1);
     }
 
     if ( !count || req.queryParams.by_member )
-        qry += (comma?"),(":"") + "for v,e,p in 2..2 inbound @user member, outbound owner filter p.vertices[1].gid == 'members' return { uid: v._key, title: v.title }";
+        qry += (comma?"),(":"") + "for v,e,p in 2..2 inbound @user member, outbound owner filter p.vertices[1].gid == 'members' return { id: v._key, title: v.title }";
 
     if ( comma )
         qry += ")) return x";
@@ -315,11 +334,11 @@ router.get('/delete', function (req, res) {
         g_db._executeTransaction({
             collections: {
                 read: ["u","admin"],
-                write: ["u","g","uuid","accn","c","d","n","a","acl","owner","ident","alias","admin","member","item","tag","note","alloc","loc"]
+                write: ["p","g","uuid","accn","c","d","n","a","acl","owner","ident","alias","admin","member","item","tag","note","alloc","loc"]
             },
             action: function() {
                 const client = g_lib.getUserFromClientID( req.queryParams.client );
-                var proj_id = "p/" + req.queryParams.id;
+                var proj_id = (req.queryParams.id.startsWith("p/")?"":"p/") + req.queryParams.id;
                 g_lib.ensureAdminPermProj( client, proj_id );
 
                 var objects;
