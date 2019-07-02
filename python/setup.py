633d8abce5186b9b91f4d39b26ee0dffffec660c
Stansberry, Dale V <stansberrydv@ornl.gov>
2019-07-02 09:50:26 -0400

    - Working on python package

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="datafed-dvstans",
     version="0.5.1",
     author="Dale Stansberry",
     author_email="stansberrydv@ornl.gov",
     description="DataFed CLI and API",
-    long_description=long_description,
+    long_description="DataFed command-line interface and client libraries.",
     long_description_content_type="text/markdown",
     url="https://github.com/ORNL/DataFed",
     packages=setuptools.find_packages(),
