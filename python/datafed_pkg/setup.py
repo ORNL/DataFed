48cd6d775c6383ca30029d42914fb16ed58605eb
Stansberry, Dale V <stansberrydv@ornl.gov>
2019-09-05 09:58:25 -0400

    - Merged CLI help with main help - Tweaked python build to create version file

--- python/datafed_pkg/setup.py
+++ python/datafed_pkg/setup.py
@@ -1,14 +1,18 @@
 import setuptools
 from os import path
-from datafed import version
 
-# read the contents of your README file
+
+# read the contents of README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
+# read the contents of VERSION file
+with open(path.join(this_directory, 'VERSION'), encoding='utf-8') as f:
+    version = f.read()
+
 setuptools.setup(
-    name="datafed-dvstans",
+    name="datafed",
     version=version,
     scripts = ['scripts/datafed'],
     author="Dale Stansberry",
@@ -18,13 +22,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ORNL/DataFed",
     packages=setuptools.find_packages(),
-    #install_requires=[
-    #    'protobuf>=3.7',
-    #    'pyzmq>=18',
-    #    'wget>=3.2',
-    #    'click>=7',
-    #    'prompt_toolkit>2'
-    #],
+    setup_requires=[
+        'setuptools',
+    ],
+    install_requires=[
+        'protobuf>=3',
+        'pyzmq>=16',
+        'wget>=3',
+        'click>=7',
+        'prompt_toolkit>=2'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
