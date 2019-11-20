84eca11a412367c56c28758515c1673754d674e5
Stansberry, Dale V <stansberrydv@ornl.gov>
2019-11-20 16:13:42 -0500

    - Forgot to add renamed globus files

---
+++ core/server/GlobusAPIClient.hpp
@@ -0,0 +1,39 @@
+#ifndef GLOBUSAPICLIENT_HPP
+#define GLOBUSAPICLIENT_HPP
+
+#include <string>
+#include <vector>
+#include <curl/curl.h>
+#include <rapidjson/document.h>
+#include "SDMS.pb.h"
+
+namespace SDMS {
+namespace Core {
+
+class GlobusAPIClient
+{
+public:
+    GlobusAPIClient();
+    ~GlobusAPIClient();
+
+    std::string getSubmissionID( const std::string & a_acc_token );
+    void        transfer( SDMS::XfrData & a_xfr, const std::string & a_acc_token );
+    bool        checkTransferStatus( const std::string & a_acc_tok, const std::string & a_task_id, SDMS::XfrStatus & a_status, std::string & a_err_msg );
+    void        cancelTask( const std::string & a_acc_tok, const std::string & a_task_id );
+    void        refreshAccessToken( const std::string & a_ref_tok, std::string & a_new_acc_tok, uint32_t & a_expires_in );
+
+private:
+    long get( const std::string & a_url, const std::string & a_token, const std::vector<std::pair<std::string,std::string>> & a_params, std::string & a_result );
+    long post( const std::string & a_url, const std::string & a_token, const std::vector<std::pair<std::string,std::string>> & a_params, const rapidjson::Document * a_body, std::string & a_result );
+    bool eventsHaveErrors( const std::vector<std::string> & a_events, SDMS::XfrStatus & status, std::string & a_err_msg );
+
+    std::string m_auth_url;
+    std::string m_xfr_url;
+    std::string m_auth_id;
+    std::string m_auth_secret;
+    CURL *      m_curl;
+};
+
+}}
+
+#endif
