ac79f980b95e529bffe927666804cccb75a60425
Dale Stansberry <stansberrydv@ornl.gov>
2020-03-16 19:15:29 -0400

    sync

--- web/static/main_browse_tab.js
+++ web/static/main_browse_tab.js
@@ -2503,8 +2503,8 @@ function makeBrowserTab(){
                 helper: function(ev){
                     var helper, node = $.ui.fancytree.getNode(ev.target), nodeTag = $(node.span);
 
-                    helper = $("<div class='fancytree-drag-helper'><span class='fancytree-drag-helper-img' /></div>")
-                    .append( nodeTag.find("span.fancytree-title").clone());
+                    helper = $("<div class='fancytree-drag-helper'><span class='fancytree-drag-helper-img' /></div>");
+                    //.append( nodeTag.find("span.fancytree-title").clone());
 
                     helper.data( "ftSourceNode", node );
 
