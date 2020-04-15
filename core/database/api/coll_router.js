0202baa83703abf4e5d86562d8b39f14b883de26
Dale Stansberry <stansberrydv@ornl.gov>
2020-04-15 10:05:42 -0400

    - Fixed issue #369

--- core/database/api/coll_router.js
+++ core/database/api/coll_router.js
@@ -294,14 +294,14 @@ router.get('/read', function (req, res) {
 
         if ( req.queryParams.offset != undefined && req.queryParams.count != undefined ){
             qry += " limit " + req.queryParams.offset + ", " + req.queryParams.count;
-            qry += " return { id: v._id, title: v.title, alias: v.alias, owner: v.owner, creator: v.creator, doi: v.doi, locked: v.locked }";
+            qry += " return { id: v._id, title: v.title, alias: v.alias, owner: v.owner, creator: v.creator, doi: v.doi, size: v.size, locked: v.locked }";
             result = g_db._query( qry, { coll: coll_id },{},{fullCount:true});
             var tot = result.getExtra().stats.fullCount;
             result = result.toArray();
             result.push({paging:{off:req.queryParams.offset,cnt:req.queryParams.count,tot:tot}});
         }
         else{
-            qry += " return { id: v._id, title: v.title, alias: v.alias, owner: v.owner, creator: v.creator, doi: v.doi, locked: v.locked }";
+            qry += " return { id: v._id, title: v.title, alias: v.alias, owner: v.owner, creator: v.creator, doi: v.doi, size: v.size, locked: v.locked }";
             result = g_db._query( qry, { coll: coll_id });
         }
 
