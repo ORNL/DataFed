5b6d4e40a8db61fcded84f471636f1e5003eb5e3
Dale Stansberry <stansberrydv@ornl.gov>
2020-08-25 18:04:29 -0400

    - New catalog view navigation working

--- web/static/panel_catalog.js
+++ web/static/panel_catalog.js
@@ -230,13 +230,16 @@ function CatalogPanel( a_id, a_frame, a_parent ){
     }
 
     function onCollectionActivate( ev ){
+        console.log("coll activate");
         var el = $(this), coll = el[0];
         //console.log("this",$(this));
+
+        $(".cat-coll-title-div,.cat-item-title",cat_coll_div).removeClass("ui-state-active");
+        $(".cat-coll-title-div",el).addClass("ui-state-active");
+
         api.collView( coll.id, function( ok, data ){
             if ( ok ){
                 panel_info.showSelectedInfo( coll.id );
-                $(".cat-coll-title-div",cat_coll_div).removeClass("ui-state-active");
-                $(".cat-coll-title-div",el).addClass("ui-state-active");
             }else{
                 dialogs.dlgAlert("Error Reading Collection",data);
             }
@@ -247,7 +250,8 @@ function CatalogPanel( a_id, a_frame, a_parent ){
 
 
     function onDataActivate( ev ){
-        var el = $(this), item = el[0], func;
+        console.log("data activate");
+        var el = $(this), item = el.parent()[0], func;
         //console.log("this",$(this));
         if ( item.id.startsWith( "c/" ))
             func = api.collView;
@@ -257,7 +261,7 @@ function CatalogPanel( a_id, a_frame, a_parent ){
         func( item.id, function( ok, data ){
             if ( ok ){
                 panel_info.showSelectedInfo( item.id );
-                $(".cat-data",cat_coll_div).removeClass("ui-state-active");
+                $(".cat-item-title,.cat-coll-title-div",cat_coll_div).removeClass("ui-state-active");
                 el.addClass("ui-state-active");
             }else{
                 dialogs.dlgAlert("Error Reading Item",data);
@@ -268,24 +272,65 @@ function CatalogPanel( a_id, a_frame, a_parent ){
     }
 
     function onCollectionOpenClose( ev ){
-        var el = $(this), coll=el.closest(".cat-coll");
+        var el = $(this),
+            coll=el.closest(".cat-coll"),
+            coll_title_div = $(".cat-coll-title-div", coll ),
+            cat_item_div = $(".cat-item-div", coll );
 
-        $(".cat-data-div", cat_coll_div ).remove();
-        topics_panel.hide();
+        if ( cat_item_div.length ){
+            coll.siblings().show();
+            topics_panel.show();
+            cat_item_div.remove();
+            $(".btn-cat-coll-open span", coll_title_div ).removeClass( "ui-icon-arrowthick-1-w" ).addClass( "ui-icon-arrowthick-1-e" );
+        }else{
+            coll.siblings().hide();
+            topics_panel.hide();
+            $(".cat-item-div", cat_coll_div ).remove();
+            coll_title_div.removeClass("ui-state-active");
+            $(".btn-cat-coll-open span", coll_title_div ).removeClass( "ui-icon-arrowthick-1-e" ).addClass( "ui-icon-arrowthick-1-w" );
 
-        coll.append("<div class='cat-data-div'>(loading...)</div>");
-        var cont = $( ".cat-data-div", coll );
+            coll.append("<div class='cat-item-div'>(loading...)</div>");
+            cat_item_div  = $( ".cat-item-div", coll );
+            cat_item_div.html( "(loading...)" );
 
-        //cont.html( "(loading...)" );
+            api.collRead( coll[0].id, 0, 100, function( ok, data ){
+                if ( ok ){
+                    setData( data, cat_item_div );
+                }else{
+                    dialogs.dlgAlert("Error Reading Collection",data);
+                    cat_item_div.html( "(faile to load colelction)" );
+                }
+            })
 
-        api.collRead( coll[0].id, 0, 100, function( ok, data ){
-            if ( ok ){
-                setData( data, cont );
-            }else{
-                dialogs.dlgAlert("Error Reading Collection",data);
-            }
-        })
+            ev.stopPropagation()
+        }
     }
+
+    function onFolderOpenClose( ev ){
+        var el = $(this),
+            coll=el.closest(".cat-item"),
+            cat_item_div = $(".cat-item-div",coll);
+
+        if ( cat_item_div.length ){
+            cat_item_div.remove();
+            coll.siblings().show();
+        }else{
+            api.collRead( coll[0].id, 0, 100, function( ok, data ){
+                if ( ok ){
+                    coll.siblings().hide();
+                    coll.append("<div class='cat-item-div'>(loading...)</div>");
+                    cat_item_div = $( ".cat-item-div", coll );
+                    //cat_item_div = coll.next();
+
+                    setData( data, cat_item_div );
+                }else{
+                    dialogs.dlgAlert("Error Reading Collection",data);
+                    cat_item_div.html( "(faile to load colelction)" );
+                }
+            });
+        }
+    }
+
 
     function setTopics( data ){
         var html = "";
@@ -323,43 +368,76 @@ function CatalogPanel( a_id, a_frame, a_parent ){
         if ( data.item && data.item.length ){
             //console.log("data",data);
             for ( var i in data.item ){
-                if ( html )
-                    html += "<hr>";
+                //if ( html )
+                //    html += "<hr>";
                 item = data.item[i];
                 html +=
                     "<div class='cat-coll' id='" + item.id + "'>\
-                        <div class='row-flex cat-coll-title-div'>\
-                            <div class='cat-coll-title'>" + item.title + "</div>\
-                            <div class='cat-coll-btn-div'>\
-                                <button class='btn btn-icon btn-cat-coll-open'><span class='ui-icon ui-icon-fullscreen'>x</span></button>\
+                        <div class='cat-coll-title-div'>\
+                            <div class='row-flex'>\
+                                <div class='cat-coll-title'>" + item.title + "</div>\
+                                <div class='cat-coll-btn-div'>\
+                                    <button class='btn btn-icon btn-cat-coll-open'><span class='ui-icon ui-icon-arrowthick-1-e'></span></button>\
+                                </div>\
                             </div>\
+                            <div>Some useful info about this collection...<br>more info</div>\
                         </div>\
                     </div>";
             }
         }else{
-            html = "<div class='cat-coll-empty'>No data collections for this topic.</div>"
+            html = "<div class='cat-coll-empty'>No data collections for this topic.<p>You may browse the catalog by selecting\
+                topics from available sub-topics (to the left), or use topic search (above) to locate specific topics.</p></div>"
         }
 
         cat_coll_div.html( html );
         $(".btn",cat_coll_div).button();
-
-        //$(".cat-topic",cat_panel).on("click", onTopicClick );
     }
 
-    function setData( a_data, a_container ){
+    function setData( a_data, a_container, a_parent ){
         console.log("setData",a_data);
-        var html = "", item;
+        var html, item;
+        /*if ( a_parent ){
+            a_parent.detach();
+        }*/
         if ( a_data.item && a_data.item.length ){
             //console.log("data",data);
+            html = ""; //"<div class='cat-item-path'>Viewing <span class='cat-coll-cur-path'>/</span></div>";
             for ( var i in a_data.item ){
                 item = a_data.item[i];
-                html += "<div class='cat-data' id='" + item.id + "'>" + item.title + "</div>";
+                if ( item.id.startsWith("d/")){
+                    html +=
+                    "<div class='cat-item' id='" + item.id + "'>\
+                        <div class='cat-item-title row-flex'>\
+                            <div style='flex:none'><span style='font-size:120%' class='ui-icon ui-icon-"+util.getDataIcon(item)+"'></span></div>\
+                            <div class='' style='flex:1 1 auto'>&nbsp;" + item.title + "</div>\
+                        </div>\
+                    </div>";
+                }else{
+                    html += 
+                    "<div class='cat-item' id='" + item.id + "'>\
+                        <div class='cat-item-title cat-folder row-flex'>\
+                            <div style='flex:none'><span style='font-size:120%' class='ui-icon ui-icon-"+util.getKeyIcon(item.id)+"'></span></div>\
+                            <div class='' style='flex:1 1 auto'>&nbsp;" + item.title + "</div>\
+                            <div style='flex:none'><button class='btn btn-icon btn-cat-folder-open'><span class='ui-icon ui-icon-arrowthick-1-e'></span></button></div>\
+                        </div>\
+                    </div>";
+                }
             }
         }else{
             html = "<div class='cat-data-empty'>No data data in this collection.</div>"
         }
 
+        /*if ( a_parent ){
+            a_container.html( "" );
+            a_container.append( a_parent );
+            a_container.append( "<div class='cat-coll-sub-items-div'>" + html + "</div>" );
+        }else{
+            a_container.html( html );
+        }*/
+
         a_container.html( html );
+       
+        $(".btn",a_container).button();
     }
 
     $(".btn-cat-home",cat_panel).on("click",function(){
@@ -473,8 +551,11 @@ function CatalogPanel( a_id, a_frame, a_parent ){
     $("#cat_topic_result_div",cat_panel).on("click", ".cat-topic", onSearchTopicClick );
     cat_coll_div.on("click", ".cat-coll", onCollectionActivate );
     cat_coll_div.on("click", ".btn-cat-coll-open", onCollectionOpenClose );
-    cat_coll_div.on("dblclick", ".cat-coll", onCollectionOpenClose );
-    cat_coll_div.on("click", ".cat-data", onDataActivate );
+    cat_coll_div.on("dblclick", ".cat-coll-title-div", onCollectionOpenClose );
+    cat_coll_div.on("click", ".cat-item-title", onDataActivate );
+    cat_coll_div.on("click", ".btn-cat-folder-open", onFolderOpenClose );
+    cat_coll_div.on("dblclick", ".cat-item-title.cat-folder", onFolderOpenClose );
+    
 
     var search_sel_mode = false;
 
