7d0cc70af3ae0761d1199ff955652efa66b22d9d
Dale Stansberry <dvstans@gmail.com>
2021-07-15 11:14:28 -0400

    Revert "- Fixed include path"
    This reverts commit c611767d2925797b6c096f25fa7f796f67940c71.

--- core/server/ClientWorker.hpp
+++ core/server/ClientWorker.hpp
@@ -6,8 +6,8 @@
 #include <thread>
 #include <algorithm>
 #include <zmq.h>
-#include "json.hpp"
-#include "json-schema.hpp"
+#include <nlohmann/json.hpp>
+#include <nlohmann/json-schema.hpp>
 #include "Util.hpp"
 #include "MsgComm.hpp"
 #include "DatabaseAPI.hpp"
