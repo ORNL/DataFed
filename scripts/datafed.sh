296b2474e8922d009edfcf40e1346ff503f6c7f9
JoshuaSBrown <joshuasbrown@yahoo.com>
2022-08-16 15:32:49 -0400

    Add example comment explaining what datafed configuration setting is doing

--- scripts/datafed.sh
+++ scripts/datafed.sh
@@ -20,6 +20,7 @@ export DATAFED_GLOBUS_APP_SECRET=""
 # ************************************************
 # Env Variables for Repo Server
 # ************************************************
+# i.e. datafed-server-test.ornl.gov:7512
 export DATAFED_SERVER_DOMAIN_NAME_AND_PORT=""
 
 # ************************************************
