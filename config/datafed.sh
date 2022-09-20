237c9b82e4d445b05df62c511cce58e61ce5fde7
Joshua S Brown <joshbro42867@yahoo.com>
2022-09-20 15:19:24 -0400

    Remove test content

--- config/datafed.sh
+++ config/datafed.sh
@@ -2,39 +2,39 @@
 
 # This is the master DataFed configuration file
 
-export DATAFED_DEFAULT_LOG_PATH="/var/log/datafed"
+export DATAFED_DEFAULT_LOG_PATH=""
 
 # ************************************************
 # Env Variables for Core Server
 # ************************************************
-export DATABASE_PASSWORD="mastermind"
+export DATABASE_PASSWORD=""
 
 # ************************************************
 # Env Variables for Web Server
 # ************************************************
-export DATAFED_ZEROMQ_SESSION_SECRET="Xgdh67s-ehEE_UI9Fx5p0hksf-u3nvls7ld8"
-export DATAFED_ZEROMQ_SYSTEM_SECRET="Xgdh67s-ehEE_UI9Fx5p0hksf-u3nvls7ld8"
+export DATAFED_ZEROMQ_SESSION_SECRET=""
+export DATAFED_ZEROMQ_SYSTEM_SECRET=""
 # An email address is required by LEGO when 
 # requesting certificates for the domain
-export DATAFED_LEGO_EMAIL="brownjs@ornl.gov"
+export DATAFED_LEGO_EMAIL=""
 
 # ************************************************
 # Env Variables for Core & Web Server
 # ************************************************
-export DATAFED_GLOBUS_APP_ID="2be799b2-f9ba-461e-8ca0-6154482e670c"
-export DATAFED_GLOBUS_APP_SECRET="xUm04Ghj9SLkiw+VKGfaNLuK8+fcNCCifzC3NwZYAxc="
+export DATAFED_GLOBUS_APP_ID=""
+export DATAFED_GLOBUS_APP_SECRET=""
 
 # ************************************************
 # Env Variables for Repo Server
 # ************************************************
 # i.e. datafed-server-test.ornl.gov:7512
-export DATAFED_SERVER_DOMAIN_NAME_AND_PORT="datafed-server-test.ornl.gov:7512"
+export DATAFED_SERVER_DOMAIN_NAME_AND_PORT=""
 
 # ************************************************
 # Env Variables for Authz, Web, Repo Server
 # ************************************************
 # If not set will resolve to datafed.ornl.gov
-export DATAFED_DOMAIN="datafed-server-test.ornl.gov"
+export DATAFED_DOMAIN=""
 
 # ************************************************
 # Env Variables for Globus Connect Server
