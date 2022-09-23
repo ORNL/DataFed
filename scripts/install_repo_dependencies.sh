957cbd8e9fb0018fec8d7ebdc3ed07ee1719d64d
JoshuaSBrown <joshuasbrown@yahoo.com>
2022-09-23 11:59:01 -0400

    Steps fixing docker builds

--- scripts/install_repo_dependencies.sh
+++ scripts/install_repo_dependencies.sh
@@ -6,48 +6,10 @@ set -e
 # This script will install all of the dependencies needed by DataFed 1.0
 sudo apt-get update
 sudo dpkg --configure -a
-sudo apt-get install -y libtool build-essential g++ gcc npm cmake libboost-all-dev pkg-config autoconf automake libtool curl make unzip libcurl4-openssl-dev libfuse-dev rapidjson-dev libkrb5-dev
+sudo apt-get install -y libtool build-essential g++ gcc cmake libboost-all-dev pkg-config autoconf automake libtool make unzip git
 sudo apt-get install -y libzmq3-dev 
 
-# The foxx services need node version 12 or greater so we aren't going to use the package manager
-# but instead will install ourselves
-
-# 1. Install nvm which will allow us to update node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm install 13
-nvm use 13
-
-if [ -d json ]
-then
-	rm -rf json
-fi
-
 cd ~
-git clone https://github.com/nlohmann/json.git
-cd json
-git checkout v3.10.2
-cmake -S . -B build
-cmake --build build -j 4
-sudo cmake --build build --target install
-cd ~
-
-if [ -d json-schema-validator ]
-then
-	rm -rf json-schema-validator
-fi
-git clone https://github.com/pboettch/json-schema-validator
-cd json-schema-validator
-git checkout 2.1.0
-cmake -S . -B build
-cmake --build build -j 4
-sudo cmake --build build --target install
-cd ~
-
 if [ -d protobuf ]
 then
 	# sudo required because of egg file
@@ -91,13 +53,4 @@ cmake -S. -B build
 cmake --build build -j 4
 sudo cmake --build build --target install
 cd ~
-
-npm install . express express-session cookie-parser helmet ini protobufjs zeromq ect client-oauth2
-
-curl -OL https://download.arangodb.com/arangodb38/DEBIAN/Release.key
-sudo apt-key add - < Release.key
-echo 'deb https://download.arangodb.com/arangodb38/DEBIAN/ /' | sudo tee /etc/apt/sources.list.d/arangodb.list
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install arangodb3
 
