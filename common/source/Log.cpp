6e33ee9b0f04b3e5384de084972b63460dd406fc
JoshuaSBrown <joshuasbrown@yahoo.com>
2023-05-17 13:50:52 -0400

    Added log functionality

---
+++ common/source/Log.cpp
@@ -0,0 +1,177 @@
+
+// Local public includes
+#include "common/Log.hpp"
+
+// Standard includes
+#include <optional>
+#include <string>
+
+namespace SDMS {
+
+  Logger global_logger;
+
+  std::string toString(const LogLevel level) {
+    if( level == LogLevel::TRACE ) {
+      return "TRACE"; 
+    } else if( level == LogLevel::DEBUG ) {
+      return "DEBUG"; 
+    } else if( level == LogLevel::INFO ) {
+      return "INFO"; 
+    } else if( level == LogLevel::WARNING ) {
+      return "WARN"; 
+    } else if( level == LogLevel::ERROR ) {
+      return "ERROR"; 
+    }  
+    return "CRIT"; 
+  }
+
+  int toSysLog(const LogLevel level) {
+    if( level == LogLevel::TRACE ) {
+      return LOG_DEBUG + 1;
+    } else if( level == LogLevel::DEBUG ) {
+      return LOG_DEBUG;
+    } else if( level == LogLevel::INFO ) {
+      return LOG_INFO;
+    } else if( level == LogLevel::WARNING ) {
+      return LOG_WARNING;
+    } else if( level == LogLevel::ERROR ) {
+      return LOG_ERR;
+    } 
+    return LOG_EMERG;
+  }
+
+  std::ostream & operator << (std::ostream &out, const LogLineContent &log_line) {
+    out << "{ ";
+      bool insert_comma = false;
+    if( not log_line.thread_name.empty() ) {
+      out << "\"thread_name\": ";
+      out << "\"" << log_line.thread_name << "\"";
+      insert_comma = true;
+    }
+    if( log_line.thread_id ) {
+      if( insert_comma ) {
+        out << ", ";
+      }
+      out << "\"thread_id\": ";
+      out << "\"" << log_line.thread_id << "\"";
+      insert_comma = true;
+    }
+    if( not log_line.correlation_id.empty() ) {
+      if( insert_comma ) {
+        out << ", ";
+      }
+      out << "\"correlation_id\": ";
+      out << "\"" << log_line.correlation_id << "\"";
+      insert_comma = true;
+    }
+    if( not log_line.message.empty() ) {
+      if( insert_comma ) {
+        out << ", ";
+      }
+      out << "\"message\": ";
+      out << "\"" << log_line.message << "\"";
+    }
+    out << " }";
+    return out;
+  }
+
+  void Logger::output(const LogLevel level, const LogLineContent & log_line, std::string file, std::string func, int line_num) {
+
+      size_t index = 0;
+      for( auto & output_stream : m_streams ) {
+        std::lock_guard<std::mutex> lock(*m_mutexes.at(index));
+        index++;
+        boost::posix_time::ptime time = boost::posix_time::microsec_clock::universal_time();
+        output_stream.get() << boost::posix_time::to_iso_extended_string(time) << "Z ";
+        output_stream.get() << toString(level) << " ";
+        output_stream.get() << file << ":" << func << ":" << line_num << " ";
+        output_stream.get() << log_line; 
+        output_stream.get() << std::endl;
+      }
+
+      if ( m_output_to_syslog ) {
+          m_buffer << file << ":" << func << ":" << line_num << " ";
+          m_buffer << log_line;
+          m_buffer << std::endl;
+          syslog(toSysLog(level),"%s",m_buffer.str().c_str());
+          m_buffer.str("");
+      } 
+  }
+
+  void Logger::setLevel(LogLevel level) noexcept { 
+    m_log_level = level;
+  }
+
+  void Logger::addStream(std::ostream & stream) {
+    m_streams.push_back(std::ref(stream));
+    m_mutexes.emplace_back(std::make_unique<std::mutex>());
+  }
+
+  void Logger::trace(const LogLineContent & content, std::string file, std::string func, int line_num) { 
+    if( m_log_level >= LogLevel::TRACE ) {
+      output(LogLevel::TRACE, content, file, func, line_num);    
+    }
+  }
+  void Logger::debug(const LogLineContent & content, std::string file, std::string func, int line_num) { 
+    if( m_log_level >= LogLevel::DEBUG ) {
+      output(LogLevel::DEBUG, content, file, func, line_num);    
+    }
+  }
+  void Logger::info(const LogLineContent & content, std::string file, std::string func, int line_num) { 
+    if( m_log_level >= LogLevel::INFO ) {
+      output(LogLevel::INFO, content, file, func, line_num);    
+    }
+  }
+  void Logger::warning(const LogLineContent & content, std::string file, std::string func, int line_num) { 
+    if( m_log_level >= LogLevel::WARNING ) {
+      output(LogLevel::WARNING, content, file, func, line_num);    
+    }
+  }
+  void Logger::error(const LogLineContent & content, std::string file, std::string func, int line_num) { 
+    if( m_log_level >= LogLevel::ERROR ) {
+      output(LogLevel::ERROR, content, file, func, line_num);    
+    }
+  }
+  void Logger::critical(const LogLineContent & content, std::string file, std::string func, int line_num) { 
+    if( m_log_level >= LogLevel::CRITICAL ) {
+      output(LogLevel::CRITICAL, content, file, func, line_num);    
+    }
+  }
+
+  void Logger::log(LogLevel level, const LogLineContent & content, std::string file, std::string func, int line_num) {
+    if( level == LogLevel::TRACE ) {
+      trace(content, file, func, line_num);
+    } else if( level == LogLevel::DEBUG ) {
+      debug(content, file, func, line_num);
+    } else if( level == LogLevel::INFO ) {
+      info(content, file, func, line_num);
+    } else if( level == LogLevel::WARNING ) {
+      warning(content, file, func, line_num);
+    } else if( level == LogLevel::ERROR ) {
+      error(content, file, func, line_num);
+    } else if( level == LogLevel::CRITICAL ) {
+      critical(content, file, func, line_num);
+    }
+  }
+/*  constexpr void output(const char * level, const LogLineContent & log_line) {
+      if ( global_log_settings.output_to_cerr ) {
+        boost::posix_time::ptime time = boost::posix_time::microsec_clock::universal_time();
+        std::cerr << boost::posix_time::to_iso_extended_string(t) << "Z\n";
+        std::cerr << std::put_time(&local_time, "%d-%m-%y %H-%M-%S ");
+        std::cerr << level << " ";
+        std::cerr << __FILE__ << ":" << __LINE__ << " ";
+        std::cerr << log_line; 
+        std::cerr << std::endl;
+      }
+
+      if ( global_log_settings.output_to_syslog ) {
+          global_log_buffer.buffer << __FILE__ << ":" << __LINE__ << " ";
+          global_log_buffer.buffer << log_line;
+          global_log_buffer.buffer << std::endl;
+          syslog(toSysLog(level),"%s",global_log_buffer.buffer.str().c_str());
+          global_log_buffer.buffer.str("");
+      } 
+  }*/
+
+} // namespace SDMS 
+
---
+++ common/tests/unit/test_Log.cpp
@@ -0,0 +1,107 @@
+#define BOOST_TEST_MAIN
+
+#define BOOST_TEST_MODULE buffer
+#include <boost/test/unit_test.hpp>
+
+// Local public includes
+#include "common/Log.hpp"
+
+// Standard includes
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <regex>
+
+using namespace SDMS;
+
+BOOST_AUTO_TEST_SUITE(LogTest)
+
+BOOST_AUTO_TEST_CASE( testing_LogOutput ) {
+
+  std::string file_name ="./log_output_test1.txt";
+  global_logger.setLevel(SDMS::LogLevel::TRACE);
+  LogLineContent log_line;
+  log_line.message = "This is a test message";
+  log_line.thread_name = "test_thread"; 
+  log_line.thread_id = 1;
+  log_line.correlation_id = "XXXXYYYY-XXXX-YYYY-XXXX-YYYYXXXXYYYY";
+
+  // Remove file if it exists
+  bool file_removed = true;
+  std::cout << "File exists" << std::endl;
+  if (std::filesystem::exists(file_name)) {
+    std::cout << "Yes" << std::endl;
+    file_removed = false;
+    if(std::filesystem::remove(file_name)) {
+      std::cout << "Removing file" << std::endl;
+      file_removed = true; 
+    }
+  }
+
+  BOOST_CHECK(file_removed);
+
+  std::ofstream file(file_name);
+  //global_log_settings.output_stream.rdbuf(file.rdbuf());
+  global_logger.addStream(file);
+  // Writing to file
+  DL_LOG_CRITICAL(log_line);
+  DL_LOG_ERROR(log_line);
+  DL_LOG_WARNING(log_line);
+  DL_LOG_INFO(log_line);
+  DL_LOG_DEBUG(log_line);
+  DL_LOG_TRACE(log_line);
+  
+  file.close();
+
+  std::ifstream file2(file_name);
+  if( !file2.is_open()) {
+    // Unable to open file
+    BOOST_CHECK(false);
+  } 
+  std::regex pattern("\"thread_id\": \"1\", \"correlation_id\": \"XXXXYYYY-XXXX-YYYY-XXXX-YYYYXXXXYYYY\"");
+
+  std::regex pattern_info("INFO");
+  std::regex pattern_debug("DEBUG");
+  std::regex pattern_trace("TRACE");
+  std::regex pattern_critical("CRIT");
+
+  bool found_info = false;
+  bool found_trace = false;
+  bool found_debug = false;
+  bool found_critical = false;
+
+  int count = 0;
+  std::string line;
+  while(std::getline(file2, line)) {
+    if( std::regex_search(line, pattern) ) {
+      ++count;
+    }
+    if( std::regex_search(line, pattern_info) ) {
+      found_info = true;
+    } else if(std::regex_search(line, pattern_debug )) {
+      found_debug = true;
+    } else if(std::regex_search(line, pattern_trace )) {
+      found_trace = true;
+    } else if(std::regex_search(line, pattern_critical )) {
+      found_critical = true;
+    }
+  }
+  file2.close();
+
+  BOOST_CHECK(found_info);
+  BOOST_CHECK(found_debug);
+  BOOST_CHECK(found_trace);
+  BOOST_CHECK(found_critical);
+  // Should be 6 of the same messages for
+  // TRACE
+  // DEBUG
+  // INFO
+  // WARN
+  // ERROR
+  // CRITICAL
+  std::cout << "Count is " << count << std::endl;
+  BOOST_CHECK(count==6);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
