b3c1d62fe8679e192389c5f2d22355d596526ea7
Brown, Joshua <brownjs@ornl.gov>
2023-06-08 22:44:09 -0400

    Fix path

--- scripts/install_repo_dependencies.sh
+++ scripts/install_repo_dependencies.sh
@@ -7,7 +7,7 @@ SCRIPT=$(realpath "$0")
 SOURCE=$(dirname "$SCRIPT")
 PROJECT_ROOT=$(realpath ${SOURCE}/..)
 
-source "${PROJECT_ROOT}/dependency_install_functions.sh"
+source "${PROJECT_ROOT}/scripts/dependency_install_functions.sh"
 
 # This script will install all of the dependencies needed by DataFed 1.0
 sudo apt-get update
