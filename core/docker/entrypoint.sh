93bf29e364772ac716835095409d21ddee7a793e
JoshuaSBrown <joshuasbrown@yahoo.com>
2023-06-26 16:59:28 -0400

    Attempt to fix core server run

--- core/docker/entrypoint.sh
+++ core/docker/entrypoint.sh
@@ -10,4 +10,21 @@ ${PROJECT_ROOT}/scripts/generate_datafed.sh
 ${PROJECT_ROOT}/scripts/generate_core_config.sh
 ${PROJECT_ROOT}/scripts/install_core.sh
 
-exec "$@"
+log_path="$DATAFED_DEFAULT_LOG_PATH"
+
+if [ "$#" -eq 0 ]; then
+  echo "No arguments were passed, running bash"
+  exec "bash"
+  exit 0
+fi
+
+datafed_core_exec=$(basename "$1")
+if [ "${datafed_core_exec}" = "datafed-core" ]
+then
+  # Send output to log file
+  "$@"  2>&1 | tee "$log_path/datafed-core.log"
+else
+  echo "Not sending output to datafed-core.log"
+  # If not do not by default send to log file
+  exec "$@"
+fi
