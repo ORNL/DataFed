60828d9a76076ed4e8ff609cadeb0d270ce29408
JoshuaSBrown <brownjs@ornl.gov>
2023-07-06 11:15:24 -0400

    Cleanup install script

--- scripts/dependency_install_functions.sh
+++ scripts/dependency_install_functions.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+SCRIPT=$(realpath "$0")
+SOURCE=$(dirname "$SCRIPT")
+source "${SOURCE}/dependency_version.sh"
 
 install_cmake() {
-  local cmake_version="3.17.5"
-  wget https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}-Linux-x86_64.tar.gz
-  tar -xzvf cmake-${cmake_version}-Linux-x86_64.tar.gz
-  cp -r cmake-${cmake_version}-Linux-x86_64/bin /usr/local
-  cp -r cmake-${cmake_version}-Linux-x86_64/share /usr/local
+  wget https://github.com/Kitware/CMake/releases/download/v${DATAFED_CMAKE_VERSION}/cmake-${DATAFED_CMAKE_VERSION}-Linux-x86_64.tar.gz
+  tar -xzvf cmake-${DATAFED_CMAKE_VERSION}-Linux-x86_64.tar.gz
+  cp -r cmake-${DATAFED_CMAKE_VERSION}-Linux-x86_64/bin /usr/local
+  cp -r cmake-${DATAFED_CMAKE_VERSION}-Linux-x86_64/share /usr/local
 
   # Cleanup
-  rm -rf cmake-${cmake_version}-Linux-x86_64 
-  rm -rf cmake-${cmake_version}-Linux-x86_64.tar.gz
+  rm -rf cmake-${DATAFED_CMAKE_VERSION}-Linux-x86_64 
+  rm -rf cmake-${DATAFED_CMAKE_VERSION}-Linux-x86_64.tar.gz
 }
 
 install_protobuf() {
-  local protobuf_version="3.17.3"
   if [ -d protobuf ]
   then
     # sudo required because of egg file
@@ -21,7 +22,7 @@ install_protobuf() {
   fi
   git clone  https://github.com/google/protobuf.git
   cd protobuf
-  git checkout v${protobuf_version}
+  git checkout v${DATAFED_PROTOBUF_VERSION}
   git submodule update --init --recursive
   cmake -S cmake/ -B build -DCMAKE_POSITION_INDEPENDENT_CODE=ON
   cmake --build build -j 4
@@ -34,14 +35,13 @@ install_protobuf() {
 }
 
 install_libsodium() {
-  local libsodium_version="1.0.18"
   if [ -d libsodium ]
   then
     rm -rf libsodium 
   fi
   git clone https://github.com/jedisct1/libsodium.git
   cd libsodium
-  git checkout "$libsodium_version"
+  git checkout "$DATAFED_LIBSODIUM_VERSION"
   ./autogen.sh
   ./configure
   make check
@@ -51,28 +51,26 @@ install_libsodium() {
 }
 
 install_libzmq() {
-  local libzmq_version="4.3.4"
   if [ -d libzmq ]
   then
     rm -rf libzmq 
   fi
   git clone https://github.com/zeromq/libzmq.git
   cd libzmq
-  git checkout v${libzmq_version}
+  git checkout v${DATAFED_LIBZMQ_VERSION}
   cmake -S. -B build
   cmake --build build -j 4
   sudo cmake --build build --target install
 }
 
 install_nlohmann_json() {
-  local nlohmann_json_version="3.10.2"
   if [ -d json ]
   then
     rm -rf json
   fi
   git clone https://github.com/nlohmann/json.git
   cd json
-  git checkout v${nlohmann_json_version}
+  git checkout v${DATAFED_NLOHMANN_JSON_VERSION}
   cmake -S . -B build
   cmake --build build -j 4
   sudo cmake --build build --target install
@@ -80,14 +78,13 @@ install_nlohmann_json() {
 }
 
 install_json_schema_validator() {
-  local json_schema_validator_version="2.1.0"
   if [ -d json-schema-validator ]
   then
     rm -rf json-schema-validator
   fi
   git clone https://github.com/pboettch/json-schema-validator
   cd json-schema-validator
-  git checkout ${json_schema_validator_version}
+  git checkout ${DATAFED_JSON_SCHEMA_VALIDATOR_VERSION}
   cmake -S . -B build
   cmake --build build -j 4
   sudo cmake --build build --target install
