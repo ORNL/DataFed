6b70ceb97d891f3f91789feb2eeaf8d6c66ce6ef
Brown, Joshua <brownjs@ornl.gov>
2023-10-18 16:39:43 -0400

    Add script for installing dependencies for building the python client

---
+++ scripts/install_python_client_dependencies.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+SCRIPT=$(realpath "$0")
+SOURCE=$(dirname "$SCRIPT")
+PROJECT_ROOT=$(realpath ${SOURCE}/..)
+
+source "${PROJECT_ROOT}/scripts/dependency_install_functions.sh"
+
+# This script will install all of the dependencies needed by DataFed 1.0
+sudo apt-get update
+sudo dpkg --configure -a
+sudo apt-get install -y libtool build-essential g++ gcc make libboost-all-dev \
+pkg-config autoconf automake unzip libcurl4-openssl-dev wget \
+rapidjson-dev libkrb5-dev git python3-pkg-resources python3-pip libssl-dev
+
+cd ~
+install_cmake
+cd ~
+
+# Install cmake 3.17
+
+python3 -m pip install --upgrade pip
+python3 -m pip install setuptools
+
+install_protobuf
+cd ~
+
+
