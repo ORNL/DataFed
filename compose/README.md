da66d18494502c71e7635bb5cf35c7e1f226251b
Brown, Joshua <brownjs@ornl.gov>
2023-10-18 15:33:08 -0400

    Initial commit for compose build

---
+++ compose/README.md
@@ -0,0 +1,4 @@
+# Compose Dev environment
+
+The files in this folder are incomplete but are the start for setting up a full
+docker compose instance of datafed.
--- compose/generate_env.sh
+++ compose/generate_env.sh
@@ -8,4 +8,8 @@ fi
 
 
 cat << EOF > ".env"
+DATAFED_DOMAIN="" # The domain of the metadata core web server
+ARANGO_ROOT_PASSWORD=""
+DATAFED_USER89_PASSWORD="" # For End to end testing
+DATAFED_REPO_FORM_PATH="" # Where the repo form is located also needed for testing
 EOF
