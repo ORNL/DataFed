f7c8b01e8b0f8c3e323d004f707d2dfd6ec614d4
Brown, Joshua <brownjs@ornl.gov>
2023-11-28 16:38:14 -0500

    Add python requirements to install

--- scripts/install_client_dependencies.sh
+++ scripts/install_client_dependencies.sh
@@ -12,7 +12,8 @@ source "${PROJECT_ROOT}/scripts/dependency_install_functions.sh"
 # This script will install all of the dependencies needed by DataFed 1.0
 sudo apt-get update
 sudo dpkg --configure -a
-# Install cmake 3.17
+
+python3 -m pip install -r ${PROJECT_ROOT}/python/datafed_pkg/requirements.txt
 
 install_protobuf
 cd ~
