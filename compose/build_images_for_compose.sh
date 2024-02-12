db16621ea738d1d25170d0c1f74c50bdc1a93e6e
Brown, Joshua <brownjs@ornl.gov>
2024-02-12 20:51:14 +0000

    Remove comments

--- compose/build_images_for_compose.sh
+++ compose/build_images_for_compose.sh
@@ -27,27 +27,3 @@ docker build -f \
   "${PROJECT_ROOT}" \
   -t datafed-web:latest
 
-
-
-## Repository server and authz library
-#docker build -f ${PROJECT_ROOT}/repository/docker/Dockerfile.repo-base.ubuntu ${PROJECT_ROOT} -t datafed/repo-base:latest
-## Retag 
-#docker tag datafed/repo-base:latest code.ornl.gov:4567/dlsw/datafed/repo-base:latest
-#docker build -f ${PROJECT_ROOT}/repository/docker/Dockerfile.repo.ubuntu ${PROJECT_ROOT} -t datafed/repo:latest
-#
-## Core and Web servers
-#REPO_LIST=("web" "core")
-#for REPO in "${REPO_LIST[@]}"
-#do
-#  CONTAINER=${REPO}
-#  docker build -f ${PROJECT_ROOT}/${REPO}/docker/Dockerfile.${REPO}-base.ubuntu ${PROJECT_ROOT} -t datafed/${CONTAINER}-base:latest
-#  # Retag 
-#  docker tag datafed/${CONTAINER}-base:latest code.ornl.gov:4567/dlsw/datafed/core-base:latest
-#  docker build -f ${PROJECT_ROOT}/${REPO}/docker/Dockerfile.${REPO}.ubuntu  ${PROJECT_ROOT} -t datafed/${CONTAINER}:latest
-#done
-#
-## Python client
-#docker build -f ${PROJECT_ROOT}/python/docker/Dockerfile.python-client-base.ubuntu ${PROJECT_ROOT} -t datafed/python-client-base:latest
-## Retag
-#docker tag datafed/python-client-base:latest code.ornl.gov:4567/dlsw/datafed/python-client:latest
-#docker build -f ${PROJECT_ROOT}/python/docker/Dockerfile.python-client.ubuntu ${PROJECT_ROOT} -t datafed/python-client:latest
