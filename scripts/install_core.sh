ffce97b0d9e004a2172a750686e93aea606ef261
Brown, Joshua <brownjs@ornl.gov>
2024-02-22 07:34:17 -0500

    Testing if keys are being created from scratch or defaults are being used

--- scripts/install_core.sh
+++ scripts/install_core.sh
@@ -4,23 +4,24 @@ set -euf -o pipefail
 
 SCRIPT=$(realpath "$0")
 SOURCE=$(dirname "$SCRIPT")
-PROJECT_ROOT=$(realpath ${SOURCE}/..)
-source ${PROJECT_ROOT}/config/datafed.sh
+PROJECT_ROOT=$(realpath "${SOURCE}/..")
+source "${PROJECT_ROOT}/config/datafed.sh"
 
 # Make sure paths exist
-mkdir -p ${DATAFED_INSTALL_PATH}/core
-mkdir -p ${DATAFED_INSTALL_PATH}/keys
-mkdir -p ${DATAFED_DEFAULT_LOG_PATH}
+mkdir -p "${DATAFED_INSTALL_PATH}/core"
+mkdir -p "${DATAFED_INSTALL_PATH}/keys"
+mkdir -p "${DATAFED_DEFAULT_LOG_PATH}"
 
 # Copy configuration files
-cp "$PROJECT_ROOT/config/datafed-core.cfg" ${DATAFED_INSTALL_PATH}/core
+cp "$PROJECT_ROOT/config/datafed-core.cfg" "${DATAFED_INSTALL_PATH}/core"
 
 
 # Move keys to Default:/opt/datafed/keys if they do not already exist
-if [ ! -f ${DATAFED_INSTALL_PATH}/keys/datafed-core-key.priv ]
+if [ ! -f "${DATAFED_INSTALL_PATH}/keys/datafed-core-key.priv" ]
 then
   # Generate keys
-  ${DATAFED_INSTALL_PATH}/core/datafed-core --gen-keys
-  mv datafed-core-key.pub ${DATAFED_INSTALL_PATH}/keys/
-  mv datafed-core-key.priv ${DATAFED_INSTALL_PATH}/keys/
+  echo "No keys for core server were detected in ${DATAFED_INSTALL_PATH}/keys/ creating them"
+  "${DATAFED_INSTALL_PATH}/core/datafed-core" --gen-keys
+  mv datafed-core-key.pub "${DATAFED_INSTALL_PATH}/keys/"
+  mv datafed-core-key.priv "${DATAFED_INSTALL_PATH}/keys/"
 fi
