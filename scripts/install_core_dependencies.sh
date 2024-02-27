827e26b2776580c7a948fdaebe6d92247126a09e
Joshua Brown <brownjs@ornl.gov>
2024-02-27 10:16:14 -0500

    Remove libzmq3-dev from apt-package installs

--- scripts/install_core_dependencies.sh
+++ scripts/install_core_dependencies.sh
@@ -10,7 +10,7 @@ PROJECT_ROOT=$(realpath ${SOURCE}/..)
 source "${PROJECT_ROOT}/scripts/utils.sh"
 source "${PROJECT_ROOT}/scripts/dependency_install_functions.sh"
 
-packages=("libtool" "build-essential" "g++" "gcc" "make" "libboost-all-dev" "pkg-config" "autoconf" "automake" "unzip" "libcurl4-openssl-dev" "wget" "rapidjson-dev" "libkrb5-dev" "git" "python3-pkg-resources" "python3-pip" "libssl-dev" "libzmq3-dev")
+packages=("libtool" "build-essential" "g++" "gcc" "make" "libboost-all-dev" "pkg-config" "autoconf" "automake" "unzip" "libcurl4-openssl-dev" "wget" "rapidjson-dev" "libkrb5-dev" "git" "python3-pkg-resources" "python3-pip" "libssl-dev")
 externals=("cmake" "nlohmann_json" "json_schema_validator" "protobuf" "libsodium" "libzmq")
 
 local_UNIFY=false
