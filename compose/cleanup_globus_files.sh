83213f25c34a06f4aae22e29bc4194f620f333e1
JoshuaSBrown <brownjs@ornl.gov>
2024-03-18 13:25:59 -0400

    Add globus cleanup files script

---
+++ compose/cleanup_globus_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+SCRIPT=$(realpath "$0")
+SOURCE=$(dirname "$SCRIPT")
+PROJECT_ROOT=$(realpath "${SOURCE}/..")
+
+# This script should be run after generating the .env file as it will pull
+# values from the .env file
+
+if [ ! -f "${PROJECT_ROOT}/compose/.env" ]
+then
+  echo "Missing . ${PROJECT_ROOT}/compose/.env file. This file needs to be"
+  echo "created first"
+  exit 1
+fi
+
+. ${PROJECT_ROOT}/compose/.env
+
+export DATAFED_GLOBUS_DEPLOYMENT_KEY_PATH="$DATAFED_HOST_DEPLOYMENT_KEY_PATH"
+export DATAFED_GLOBUS_CRED_FILE_PATH="$DATAFED_HOST_CRED_FILE_PATH"
+
+if [ -f "$DATAFED_HOST_CRED_FILE_PATH" ]
+then
+  export GCS_CLI_CLIENT_ID=$(cat ${DATAFED_HOST_CRED_FILE_PATH}  | jq -r .client)
+  export GCS_CLI_CLIENT_SECRET=$(cat ${DATAFED_HOST_CRED_FILE_PATH}  | jq -r .secret)
+fi
+
+if [ -f "$DATAFED_GLOBUS_DEPLOYMENT_KEY_PATH" ]
+then
+  export GCS_CLI_ENDPOINT_ID=$(cat ${DATAFED_GLOBUS_DEPLOYMENT_KEY_PATH}  | jq -r .client_id)
+fi
+
+sudo globus-connect-server node cleanup
+
+python3 "${PROJECT_ROOT}/scripts/globus/globus_cleanup.py"
+
+#rm -rf "${PROJECT_ROOT}/compose/globus"
