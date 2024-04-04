dae37d9a4d0b9d235e82fe0a4a78cd4ec4aac0a1
Brown, Joshua <brownjs@ornl.gov>
2024-04-04 08:35:08 -0400

    Create tests for functions that will check ftp:// prefix

---
+++ repository/gridftp/globus5/authz/source/URL.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Function to extract the relative path from an FTP URL
+ *
+ * Will take a uri of the form ftp://domain/path/to/file
+ *
+ * If the URI has the prefix
+ *
+ * ftp://domain Returns 1 for success
+ *              else it will return 0 for failure
+ **/
+int ftpExtractRelativePath(const char *url, char *relativePath, size_t maxLength);
+
+/**
+ * Will compare two strings and ensure that prefixes are equivalent
+ *
+ * On success will return the results of strncmp which will be 0 if they 
+ * match.
+ **/
+int comparePrefix(const char *str1, const char *str2, size_t prefix_length);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ repository/gridftp/globus5/authz/tests/unit/test_URL.cpp
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BOOST_TEST_MAIN
+
+#define BOOST_TEST_MODULE buffer
+#include <boost/filesystem.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "URL.h"
+
+BOOST_AUTO_TEST_SUITE(BufferTest)
+
+BOOST_AUTO_TEST_CASE(test_1_ftpExtractRelativePath) {
+	char relative_path[100]; // Adjust the buffer size as needed
+	const char *url1 = "ftp://domain/relative_path";
+
+  int rv = ftpExtractRelativePath(url1, relative_path, sizeof(relative_path));
+  // Should pass
+  BOOST_CHECK(strcmp(relative_path, "/relative_path") == 0);
+  BOOST_CHECK(rv == 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_2_ftpExtractRelativePath) {
+	char relative_path[100]; // Adjust the buffer size as needed
+	const char *url = "ftp://domain";
+
+  int rv = ftpExtractRelativePath(url, relative_path, sizeof(relative_path));
+  // Should not throw an error
+  BOOST_CHECK( rv == 1);
+  BOOST_CHECK( strcmp(relative_path, "/") == 0);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_3_ftpExtractRelativePath) {
+	char relative_path[100]; // Adjust the buffer size as needed
+	const char *url = "";
+
+  int rv = ftpExtractRelativePath(url, relative_path, sizeof(relative_path));
+  // Should throw an error
+  BOOST_CHECK( rv == 0);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_4_ftpExtractRelativePath) {
+	char relative_path[100]; // Adjust the buffer size as needed
+	const char *url = "ftp:///";
+
+  int rv = ftpExtractRelativePath(url, relative_path, sizeof(relative_path));
+  // Should throw an error
+  BOOST_CHECK( rv == 0);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_5_ftpExtractRelativePath) {
+	char relative_path[100]; // Adjust the buffer size as needed
+	const char *url = "ftp:/domain///path";
+
+  int rv = ftpExtractRelativePath(url, relative_path, sizeof(relative_path));
+  // Should throw an error because prefix is incorrect
+  BOOST_CHECK( rv == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_6_ftpExtractRelativePath) {
+	char relative_path[100]; // Adjust the buffer size as needed
+	const char *url = "ftp://domain///path";
+
+  int rv = ftpExtractRelativePath(url, relative_path, sizeof(relative_path));
+  // Should not throw an error
+  BOOST_CHECK( rv == 1);
+  printf("Relative path is %s\n", relative_path);
+  BOOST_CHECK(strcmp(relative_path, "///path") == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_1_comparePrefix){
+	const char * allowed_prefix="/mnt/storage/globus";
+  size_t prefix_len = strlen(allowed_prefix);
+  const char * relative_path="";
+  int rv = comparePrefix(allowed_prefix, relative_path, prefix_len);
+
+  // Should fail
+  BOOST_CHECK(rv != 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_2_comparePrefix){
+	const char * allowed_prefix="/mnt/storage/globus";
+  size_t prefix_len = strlen(allowed_prefix);
+  const char * relative_path="/";
+  int rv = comparePrefix(allowed_prefix, relative_path, prefix_len);
+
+  // Should fail
+  BOOST_CHECK(rv != 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_3_comparePrefix){
+	const char * allowed_prefix="/mnt/storage/globus";
+  size_t prefix_len = strlen(allowed_prefix);
+  const char * relative_path="/mnt/storage/globus/";
+  int rv = comparePrefix(allowed_prefix, relative_path, prefix_len);
+
+  // Should pass
+  BOOST_CHECK(rv == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_4_comparePrefix){
+	const char * allowed_prefix="/mnt/storage/globus";
+  size_t prefix_len = strlen(allowed_prefix);
+  const char * relative_path="/mnt/storage/globus";
+  int rv = comparePrefix(allowed_prefix, relative_path, prefix_len);
+
+  // Should pass
+  BOOST_CHECK(rv == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_5_comparePrefix){
+	const char * allowed_prefix="/mnt/storage/globus";
+  size_t prefix_len = strlen(allowed_prefix);
+  const char * relative_path="/mnt/storage/globu";
+  int rv = comparePrefix(allowed_prefix, relative_path, prefix_len);
+
+  // Should fail
+  BOOST_CHECK(rv != 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
