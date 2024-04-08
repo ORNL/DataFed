845b6ea1d6540d1b601a30590783206accf50915
Joshua Brown <brownjs@ornl.gov>
2024-04-08 07:56:43 -0400

    Remove the chmod commands from foxx ephemeral container

--- docker/entrypoint_foxx.sh
+++ docker/entrypoint_foxx.sh
@@ -2,28 +2,34 @@
 
 set -euf -o pipefail
 
-if [ -n "$UID" ]; then
-    usermod -u $UID datafed
-fi
+# NOTE We do not need to change the user in this container because we should not have
+# any log output, and running chmod to update all the folders during runtime is
+# expensive!
+# 
+# The following lines are not needed
+# if [ -n "$UID" ]; then
+#     usermod -u $UID datafed
+# fi
 
 SCRIPT=$(realpath "$0")
 SOURCE=$(dirname "$SCRIPT")
 PROJECT_ROOT=$(realpath "${SOURCE}/../")
 
-log_path="$DATAFED_DEFAULT_LOG_PATH"
-if [ ! -d "${log_path}" ]
-then
-  su -c "mkdir -p ${log_path}" datafed
-fi
-
-cd "${PROJECT_ROOT}"
-# Check to see if foxx has previously been installed
 install_flag="/tmp/.foxx_is_installed"
 if [ ! -f "$install_flag" ]
 then
-  su datafed -c "${PROJECT_ROOT}/scripts/generate_datafed.sh"
+  log_path="$DATAFED_DEFAULT_LOG_PATH"
+  if [ ! -d "${log_path}" ]
+  then
+    su -c "mkdir -p ${log_path}" datafed
+  fi
 
-  su datafed -c "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake -S. -B build						\
+  # It should be fine to run this as root because it is an ephemeral container anyway
+  cd "${PROJECT_ROOT}"
+  # Check to see if foxx has previously been installed
+  "${PROJECT_ROOT}/scripts/generate_datafed.sh"
+
+  "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" -S. -B build						\
     -DBUILD_REPO_SERVER=False		\
     -DBUILD_COMMON=False        \
     -DBUILD_AUTHZ=False					\
@@ -32,14 +38,15 @@ then
     -DBUILD_DOCS=False					\
     -DBUILD_PYTHON_CLIENT=False	\
     -DBUILD_FOXX=True           \
-    -DINSTALL_FOXX=True"
+    -DINSTALL_FOXX=True
 
-  su datafed -c "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake --build build"
+
+  "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" --build build
 
   # Give arango container a minute to initialize
   # should be replaced with health check at some point
   sleep 5
-  su datafed -c "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake --build build --target install"
+  "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" --build build --target install
   
   touch "$install_flag"
 fi
