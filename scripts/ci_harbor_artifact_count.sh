473ef8732e8914b31218d7075d7eb1e0d3e445ba
JoshuaSBrown <brownjs@ornl.gov>
2024-06-17 05:51:27 -0400

    Fix passing string instead of variable in ci file

--- scripts/ci_harbor_artifact_count.sh
+++ scripts/ci_harbor_artifact_count.sh
@@ -2,6 +2,8 @@
 
 # This script is created to determine if a harbor image exists in the repository
 # It will print the number of artifacts that exist for a particualar image.
+LOG_FILE="harbor_check.log"
+echo "CI Harbor Check Log File" > "$LOG_FILE"
 
 if [ -z "${DATAFED_HARBOR_REGISTRY}" ]
 then
@@ -50,42 +52,43 @@ fi
 
 if [ -z "$local_DATAFED_HARBOR_REGISTRY" ]
 then
-  echo "The DATAFED Harbor Registry has not been defined."
+  echo "The DATAFED Harbor Registry has not been defined." >> "$LOG_FILE"
   exit 1
 fi
 
 if [ -z "$local_DATAFED_HARBOR_REPOSITORY" ]
 then
-  echo "The DATAFED Harbor Repository has not been defined."
+  echo "The DATAFED Harbor Repository has not been defined." >> "$LOG_FILE"
   exit 1
 fi
 
 if [ -z "$local_DATAFED_HARBOR_USERNAME" ]
 then
-  echo "The DATAFED Harbor Username has not been defined."
+  echo "The DATAFED Harbor Username has not been defined." >> "$LOG_FILE"
   exit 1
 fi
 
 if [ -z "$local_DATAFED_HARBOR_PASSWORD" ]
 then
-  echo "The DATAFED Harbor Password has not been defined."
+  echo "The DATAFED Harbor Password has not been defined." >> "$LOG_FILE"
   exit 1
 fi
 
 URL="$local_DATAFED_HARBOR_URL/api/v2.0/projects/$local_DATAFED_HARBOR_PROJECT/repositories/$local_DATAFED_HARBOR_REPOSITORY/artifacts"
+echo "URL" >> "$LOG_FILE"
 data=$(curl -u $local_DATAFED_HARBOR_USERNAME:$local_DATAFED_HARBOR_PASSWORD -s "$URL?with_tag=$local_DATAFED_HARBOR_IMAGE_TAG" )
-
 error_code=$(echo $?)
 if [ "$error_code" != "0" ]
 then
-	echo "Something went wrong when communicating with harbor registry"
-	echo "$URL with tag $local_DATAFED_HARBOR_IMAGE_TAG"
+	echo "Something went wrong when communicating with harbor registry" >> "$LOG_FILE"
+
+	echo "$URL with tag $local_DATAFED_HARBOR_IMAGE_TAG" >> "$LOG_FILE"
 	if [ "$error_code" == "6" ]
 	then
-		echo "cURL error code 6, could not resolve host, make sure the domain is correct $local_DATAFED_HARBOR_URL, and that the network is open, and that the site is up."
+		echo "cURL error code 6, could not resolve host, make sure the domain is correct $local_DATAFED_HARBOR_URL, and that the network is open, and that the site is up." >> "$LOG_FILE"
 	elif [ "$error_code" == "1" ]
 	then
-		echo "cURL error code 1, generic error code detected, make sure the password username combination is correct."
+		echo "cURL error code 1, generic error code detected, make sure the password username combination is correct." >> "$LOG_FILE"
 	fi
 fi
 number_of_artifacts=$(echo "$data" | jq ' . | length')
