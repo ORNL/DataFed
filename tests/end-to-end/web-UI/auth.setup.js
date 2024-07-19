46f251f682d7b2dea4655d84c44aff21ee339fa8
dingc2 <cheng.ding@vanderbilt.edu>
2024-07-19 09:34:32 -0400

    added some tests

---
+++ tests/end-to-end/web-UI/auth.setup.js
@@ -0,0 +1,53 @@
+const { webkit } = require('playwright');
+const path = require('path');
+const process = require('process');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+console.log("******Inside Setup file******");
+
+module.exports = async function ({ page }) {
+    // if a playwright page object doesn't exist, create one
+    if (!page) {
+        const browser = await webkit.launch({
+            args: ['--ignore-certificate-errors'],
+            timeout: 30000,
+        });
+        const context = await browser.newContext({
+            ignoreHTTPSErrors: true,
+        });
+
+        page = await context.newPage();  
+        console.log("new page and browser object created");
+    }
+
+    // Go to the website and login through globus using a tester account
+    await page.goto('https://localhost/ui/welcome');//TESTING
+    if (await page.getByRole('button', { name: 'Log In / Register' }).isVisible()) {
+        await page.getByRole('button', { name: 'Log In / Register' }).click();
+        if (page.getByRole('link', { name: 'Globus globus' }).isVisible()) {
+            page.getByRole('button', { name: 'Globus ID to sign in' }).click();
+            if (await page.getByLabel('Username @globusid.org').isEditable()) {
+                // changes the username and password in the .env file if needed
+                await page.getByLabel('Username @globusid.org').fill(process.env.TEST_USERNAME);
+                await page.getByLabel('Password').fill(process.env.TEST_PASSWORD);
+                await page.getByRole('button', { name: 'Log In' }).click();
+                console.log("******PAST LOGIN******");
+                await page.context().storageState({ path: './tests/end-to-end/web-UI/.auth/user.json'}); //TESTING
+                console.log("******Done with login******");
+            } else {
+                console.log("DID NOT SEE FORM");
+            }
+        } else {
+            console.log("DID NOT SEE GLOBUS BUTTON");
+        } 
+    } else {
+        console.log("DID NOT SEE LOGIN BUTTON");
+    }
+    
+    // return page;  // pass on the page variable if using this function directly in a test script
+};
+
+// TODO this is not as efficient as storing the states in the context variable,
+// something like "page.context().storagestate" would work better in the future when
+// there are many more test files
+
--- tests/end-to-end/web-UI/playwright.config.js
+++ tests/end-to-end/web-UI/playwright.config.js
@@ -2,24 +2,22 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
+  //If the server is slow, consider increasing the timeout (in ms)
+  timeout: 30000,
+
+  globalSetup: require.resolve('./auth.setup'),
   testDir: './scripts',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -29,10 +27,13 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
     ignoreHTTPSErrors: true,
+    
     launchOptions: {
       args: ['--ignore-certificate-errors'],
     },
+    storageState: 'tests/end-to-end/web-UI/.auth/user.json',
   },
   
 
@@ -40,18 +41,35 @@ module.exports = defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'setup',
+      testMatch: 'auth.setup.js',
+    },
+
+    {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: './tests/end-to-end/web-UI/.auth/user.json',
+      },
+       dependencies: ['setup'],
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        ...devices['Desktop Firefox'],
+        // storageState: './tests/end-to-end/web-UI/.auth/user.json',
+      },
+      // dependencies: ['setup'],
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        ...devices['Desktop Safari'],
+        // storageState: './tests/end-to-end/web-UI/.auth/user.json',
+       },
+      // dependencies: ['setup'],
     },
 
     /* Test against mobile viewports. */
