0b874a20da4ae722516829813e0e44f577b0467a
dingc2 <cheng.ding@vanderbilt.edu>
2024-08-01 14:44:15 -0400

    fixing domain issue

--- tests/end-to-end/web-UI/scripts/testingCollectionCreation.spec.js
+++ tests/end-to-end/web-UI/scripts/testingCollectionCreation.spec.js
@@ -3,19 +3,24 @@ import { test, expect } from '@playwright/test';
 
 
 test('create a collection', async ({page}) => {
-    // 1. Login to DataFed
-    // 2. Click on a Collection in user/project space
-    // 3. Create new collection
-    // 4. Enter a title
-    // 5. Press Create
-    await page.goto('/');
-    await page.getByText('Root Collection').click();
-    await page.getByText('Root Collectionc/').click({
-      button: 'right'
-    });
-    await page.getByRole('menuitem', { name: ' New' }).click();
-    await page.getByRole('menuitem', { name: 'Collection' }).click();
-    await page.locator('#title').fill('TestCollection');
-    await page.getByRole('button', { name: 'Create' }).click();
-    await page.getByLabel('Confirm Deletion').getByRole('button', { name: 'Delete' }).click();
+  // 1. Login to DataFed
+  // 2. Click on a Collection in user/project space
+  // 3. Create new collection
+  // 4. Enter a title
+  // 5. Press Create
+  await page.goto('/');
+  await page.getByText('Root Collection').click();
+  await page.getByText('Root Collectionc/').click({
+    button: 'right'
+  });
+  await page.getByRole('menuitem', { name: ' New' }).click();
+  await page.getByRole('menuitem', { name: 'Collection' }).click();
+  await page.locator('#title').fill('TestCollection');
+  await page.getByRole('button', { name: 'Create' }).click();
+  await page.getByText('TestCollectionc/').click({ button: "right" });
+  await page.getByRole('menuitem', { name: ' Actions' }).click();
+  await page.getByRole('menuitem', { name: 'Delete' }).click();
+  await page.getByLabel('Confirm Deletion').getByRole('button', { name: 'Delete' }).click();
+  
+  //assert record is deleted
 });
