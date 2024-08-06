e16bda55e9342b7233674b50fd5eb973201a21b6
dingc2 <cheng.ding@vanderbilt.edu>
2024-08-06 15:13:28 -0400

    fixing stuff?

--- tests/end-to-end/web-UI/scripts/test4SchemaCreation.spec.js
+++ tests/end-to-end/web-UI/scripts/test4SchemaCreation.spec.js
@@ -3,8 +3,8 @@ import { test, expect } from '@playwright/test';
 test('test schema creation', async ({ page }) => {
     await page.goto('/'); // baseURL is in playwright.config
     await page.locator("#btn_schemas").click();
-    await page.locator("#btn_schemas").click({timeout: 20000}); // Only clicking twice opens it, fix this later plz
-    await expect(page.getByText('Manage Schemas')).toBeVisible({timeout: 20000});
+    await page.locator("#btn_schemas").click({ timeout: 20000 }); // Only clicking twice opens it, fix this later plz
+    await expect(page.getByText('Manage Schemas')).toBeVisible({ timeout: 20000 });
     await expect(page.getByText('(no matches)')).toBeVisible();
     await page.getByRole('button', { name: 'New' }).click();
     await page.getByRole('textbox', { name: 'Schema ID' }).fill('123456789');
@@ -18,7 +18,7 @@ test('test schema creation', async ({ page }) => {
     await page.getByTitle('Delete schema').click();
     await page.getByRole('button', { name: 'Close', exact: true }).click();
     await page.locator("#btn_schemas").click();
-    await expect(page.getByText('(no matches)')).toBeVisible();
+    await expect(page.getByText('(no matches)')).toBeVisible({timeout: 20000});
     await page.getByRole('button', { name: 'Close', exact: true }).click();
 
 });
