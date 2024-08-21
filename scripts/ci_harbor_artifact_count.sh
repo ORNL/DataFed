0874d8f07558c86b7589af868dad9e216b739360
JoshuaSBrown <brownjs@ornl.gov>
2024-08-21 14:36:13 -0400

    Address code reviews

--- scripts/ci_harbor_artifact_count.sh
+++ scripts/ci_harbor_artifact_count.sh
@@ -7,13 +7,6 @@ set -euf
 LOG_FILE="harbor_check.log"
 echo "CI Harbor Check Log File" > "$LOG_FILE"
 
-if [ -z "${DATAFED_HARBOR_REGISTRY}" ]
-then
-  local_DATAFED_HARBOR_REGISTRY=""
-else
-  local_DATAFED_HARBOR_REGISTRY=$(printenv DATAFED_HARBOR_REGISTRY)
-fi
-
 local_DATAFED_HARBOR_URL="https://$local_DATAFED_HARBOR_REGISTRY"
 
 if [ -z "${DATAFED_HARBOR_PROJECT}" ]
@@ -23,14 +16,6 @@ else
   local_DATAFED_HARBOR_PROJECT=$(printenv DATAFED_HARBOR_PROJECT)
 fi
 
-# Should be the name of the image
-if [ -z "${DATAFED_HARBOR_REPOSITORY}" ]
-then
-  local_DATAFED_HARBOR_REPOSITORY=""
-else
-  local_DATAFED_HARBOR_REPOSITORY=$(printenv DATAFED_HARBOR_REPOSITORY)
-fi
-
 if [ -z "${DATAFED_HARBOR_IMAGE_TAG}" ]
 then
   local_DATAFED_HARBOR_IMAGE_TAG="latest"
@@ -38,43 +23,22 @@ else
   local_DATAFED_HARBOR_IMAGE_TAG=$(printenv DATAFED_HARBOR_IMAGE_TAG)
 fi
 
-if [ -z "${DATAFED_HARBOR_USERNAME}" ]
-then
-  local_DATAFED_HARBOR_USERNAME=""
-else
-  local_DATAFED_HARBOR_USERNAME=$(printenv DATAFED_HARBOR_USERNAME)
-fi
+# Define an array with variable names
+vars=("DATAFED_HARBOR_REGISTRY" "DATAFED_HARBOR_REPOSITORY" "DATAFED_HARBOR_USERNAME" "DATAFED_HARBOR_PASSWORD")
 
-if [ -z "${DATAFED_HARBOR_PASSWORD}" ]
-then
-  local_DATAFED_HARBOR_PASSWORD=""
-else
-  local_DATAFED_HARBOR_PASSWORD=$(printenv DATAFED_HARBOR_PASSWORD)
-fi
+# Loop through the array and set local variables
+for var in "${vars[@]}"; do
+  eval "local_${var}=\${${var}:-\"\"}"
+done
 
-if [ -z "$local_DATAFED_HARBOR_REGISTRY" ]
-then
-  echo "The DATAFED Harbor Registry has not been defined." >> "$LOG_FILE"
-  exit 1
-fi
-
-if [ -z "$local_DATAFED_HARBOR_REPOSITORY" ]
-then
-  echo "The DATAFED Harbor Repository has not been defined." >> "$LOG_FILE"
-  exit 1
-fi
-
-if [ -z "$local_DATAFED_HARBOR_USERNAME" ]
-then
-  echo "The DATAFED Harbor Username has not been defined." >> "$LOG_FILE"
-  exit 1
-fi
-
-if [ -z "$local_DATAFED_HARBOR_PASSWORD" ]
-then
-  echo "The DATAFED Harbor Password has not been defined." >> "$LOG_FILE"
-  exit 1
-fi
+# Check for required variables
+for var in "${vars[@]}"; do
+  local_var="local_${var}"
+  if [ -z "${!local_var}" ]; then
+    echo "The ${var} has not been defined." >> "$LOG_FILE"
+    exit 1
+  fi
+done
 
 URL="$local_DATAFED_HARBOR_URL/api/v2.0/projects/$local_DATAFED_HARBOR_PROJECT/repositories/$local_DATAFED_HARBOR_REPOSITORY/artifacts"
 echo "$URL" >> "$LOG_FILE"
