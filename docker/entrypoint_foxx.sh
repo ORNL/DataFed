aee3dd51167d15cc0821c7fef828a090cbc1e035
JoshuaSBrown <brownjs@ornl.gov>
2024-10-30 22:39:21 -0400

    Support running foxx tests as part of foxx install container

--- docker/entrypoint_foxx.sh
+++ docker/entrypoint_foxx.sh
@@ -28,33 +28,56 @@ then
     su -c "mkdir -p ${log_path}" datafed
   fi
   
-  # It should be fine to run this as root because it is an ephemeral container anyway
+  # It should be fine to run this as root because it is an ephemeral container
+  # anyway
   cd "${PROJECT_ROOT}"
   # Check to see if foxx has previously been installed
   "${PROJECT_ROOT}/scripts/generate_datafed.sh"
 
-  "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" -S. -B build						\
-    -DBUILD_REPO_SERVER=False		\
-    -DBUILD_COMMON=False        \
-    -DBUILD_AUTHZ=False					\
-    -DBUILD_CORE_SERVER=False		\
-    -DBUILD_WEB_SERVER=False		\
-    -DBUILD_DOCS=False					\
-    -DBUILD_PYTHON_CLIENT=False	\
-    -DBUILD_FOXX=True           \
-    -DINSTALL_FOXX=True
-
+  if [ "$ENABLE_FOXX_TESTS" == "TRUE" ]
+  then
+    "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" -S. -B build            \
+      -DBUILD_REPO_SERVER=False   \
+      -DBUILD_COMMON=False        \
+      -DBUILD_AUTHZ=False         \
+      -DBUILD_CORE_SERVER=False   \
+      -DBUILD_WEB_SERVER=False    \
+      -DBUILD_DOCS=False          \
+      -DBUILD_PYTHON_CLIENT=False \
+      -DBUILD_FOXX=True           \
+      -DINSTALL_FOXX=True         \
+      -DENABLE_FOXX_TESTS=True
+  else
+    "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" -S. -B build            \
+      -DBUILD_REPO_SERVER=False   \
+      -DBUILD_COMMON=False        \
+      -DBUILD_AUTHZ=False         \
+      -DBUILD_CORE_SERVER=False   \
+      -DBUILD_WEB_SERVER=False    \
+      -DBUILD_DOCS=False          \
+      -DBUILD_PYTHON_CLIENT=False \
+      -DBUILD_FOXX=True           \
+      -DINSTALL_FOXX=True
+  fi
 
   "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" --build build
 
   # Give arango container a minute to initialize
   # should be replaced with health check at some point
   sleep 5
-  "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" --build build --target install
+  "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" \
+    --build build \
+    --target install
   
   touch "$install_flag"
   chown -R "$UID":"$UID" "/tmp"
 
+  if [ "$ENABLE_FOXX_TESTS" == "TRUE" ]
+  then
+    "${DATAFED_DEPENDENCIES_INSTALL_PATH}/bin/cmake" \
+      --build build \
+      --target test
+  fi
 else
   echo "/tmp/.foxx_is_installed has been found skipping reinstall"
 fi
