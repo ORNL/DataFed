deecc353a2a3edebdfd5734d2029b7f21ecc3d59
Blake Nedved <nedvedba@ornl.gov>
2024-11-12 05:00:36 -0500

    added function definitions and initial per test case setup

--- core/server/tests/unit/test_GlobusAPI.cpp
+++ core/server/tests/unit/test_GlobusAPI.cpp
@@ -4,6 +4,7 @@
 
 // Local private includes
 #include "GlobusAPI.hpp"
+#include "Config.hpp"
 
 // Local public includes
 #include "common/DynaLog.hpp"
@@ -25,28 +26,38 @@ using namespace SDMS::Core;
 
 class TestGlobusAPI: public GlobusAPI {
 public:
-  TestGlobusAPI();
-  explicit TestGlobusAPI(LogContext log_context);
+  TestGlobusAPI() {
+    GlobusAPI::init()
+  }
 
-  ~TestGlobusAPI();
+  ~TestGlobusAPI() {}
   
   long get(CURL *a_curl, const std::string &a_base_url,
            const std::string &a_url_path, const std::string &a_token,
            const std::vector<std::pair<std::string, std::string>> &a_params,
-           std::string &a_result);
+           std::string &a_result) {
+        return GlobusAPI::get(a_curl, a_base_url, a_url_path, a_token, a_params, a_result);
+    }
+    
   long post(CURL *a_curl, const std::string &a_base_url,
             const std::string &a_url_path, const std::string &a_token,
             const std::vector<std::pair<std::string, std::string>> &a_params,
-            const libjson::Value *a_body, std::string &a_result);
+            const libjson::Value *a_body, std::string &a_result) {
+        return GlobusAPI::post(a_curl, a_base_url, a_url_path, a_token, a_params, a_body, a_result);
+    }
 };
 
 BOOST_AUTO_TEST_SUITE(GlobusAPITest)
 
 BOOST_AUTO_TEST_CASE(testing_GlobusAPIPost) {
+    Config config = Config::getInstance();
+    TestGlobusAPI api();
     BOOST_TEST(true);
 }
 
 BOOST_AUTO_TEST_CASE(testing_GlobusAPIGet) {
+    Config config = Config::getInstance();
+    TestGlobusAPI api();
     BOOST_TEST(true);
 }
 
