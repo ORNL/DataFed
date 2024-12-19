1dddc59d1b358c98991dc27516972bb4a2250e20
JoshuaSBrown <brownjs@ornl.gov>
2024-12-19 15:59:35 -0500

    Apply formatting

--- core/database/foxx/api/repo.js
+++ core/database/foxx/api/repo.js
@@ -25,12 +25,12 @@ const pathModule = require("./posix_path");
  * REPO_ROOT_PATH = /mnt/science/datafed
  **/
 class PathType {
-  PROJECT_PATH: "PROJECT_PATH",
-  USER_PATH: "USER_PATH",
-  RECORD_PATH: "RECORD_PATH",
-  REPO_BASE_PATH: "REPO_BASE_PATH",
-  REPO_ROOT_PATH: "REPO_PATH",
-  UNKNOWN: "UNKNOWN"
+PROJECT_PATH: "PROJECT_PATH",
+                USER_PATH: "USER_PATH",
+                RECORD_PATH: "RECORD_PATH",
+                REPO_BASE_PATH: "REPO_BASE_PATH",
+                REPO_ROOT_PATH: "REPO_PATH",
+                UNKNOWN: "UNKNOWN"
 }
 
 class Repo {
@@ -63,10 +63,10 @@ class Repo {
     if (a_key && a_key !== "repo/" ) {
       if ( a_key.startsWith("repo/") ) {
         this.#repo_id = a_key;
-	this.#repo_key = a_key.slice("repo/".length);
+        this.#repo_key = a_key.slice("repo/".length);
       } else {
         this.#repo_id = "repo/" + a_key;
-	this.#repo_key = a_key;
+        this.#repo_key = a_key;
       }
 
       // Check if the repo document exists
@@ -121,65 +121,66 @@ class Repo {
   }
 
 
-	/**
-	 * @brief Detect what kind of POSIX path has been provided
-	 **/
-	pathType(a_path) {
-		if ( !this.#exists ) {
-			// Should throw an error because the repo is not valid
-		}
-		let repo = db._document(this.#repo_id);
+  /**
+   * @brief Detect what kind of POSIX path has been provided
+   **/
+  pathType(a_path) {
+    if ( !this.#exists ) {
+      // Should throw an error because the repo is not valid
+      throw [g_lib.ERR_PERM_DENIED, "Record does not exist " + this.#repo_id]
+    }
+    let repo = db._document(this.#repo_id);
 
-		let repo_root_path = repo.path;
-		if ( repo_root_path.endsWith("/")) {
-			repo_root_path.slice(0, -1);
-		}
+    let repo_root_path = repo.path;
+    if ( repo_root_path.endsWith("/")) {
+      repo_root_path.slice(0, -1);
+    }
 
-		let sanitized_path = a_path;
-		if( sanitized_path.endsWith("/")) {
-			sanitized_path.slice(0, -1);
-		}
+    let sanitized_path = a_path;
+    if( sanitized_path.endsWith("/")) {
+      sanitized_path.slice(0, -1);
+    }
 
-		// Make sure that the provided path begins with the repo root path
-		// path/repo_root       Valid
-		// path/repo_root/foo   Valid 
-		// path/repo_root_bar   Invalid
-		if( sanitized_path.length === repo_root_path.length )  {
-			if( sanitized_path !== repo_root_path) {
-				return PathType.UNKNOWN;
-			} else {
-				return PathType.REPO_PATH;
-			}
-		} else if( ! sanitized_path.beginsWith(repo_root_path + "/") {
-			return PathType.UNKNOWN;
-		}
+    // Make sure that the provided path begins with the repo root path
+    // path/repo_root       Valid
+    // path/repo_root/foo   Valid 
+    // path/repo_root_bar   Invalid
+    if( sanitized_path.length === repo_root_path.length )  {
+      if( sanitized_path !== repo_root_path) {
+        return PathType.UNKNOWN;
+      } else {
+        return PathType.REPO_PATH;
+      }
+    } else if( ! sanitized_path.beginsWith(repo_root_path + "/")) {
+      return PathType.UNKNOWN;
+    }
 
-			const relative_path = sanitized_path.substr(repo_root_path.length); 
+    const relative_path = sanitized_path.substr(repo_root_path.length); 
 
-			const relative_path_components = pathModule.splitPOSIXPath(relative_path_components);
+    const relative_path_components = pathModule.splitPOSIXPath(relative_path_components);
 
-			// Check if is valid project
-			if ( relative_path_components[0] === "project" ) {
-				if (relative_path_components.length === 1) {
-					// REPO_PATH , PROJECT_PATH is reserved to project/<project_name>/<id>
-					return PathType.REPO_PATH;
-				}else (relative_path_components.length > 1) {
-					return PathType.PROJECT_PATH;
-				}
-			} else if( relative_path_components[0] === "user" ) {
-				// Check if valid user
-				if (relative_path_components.length === 1) {
-					// REPO_PATH , PROJECT_PATH is reserved to project/<project_name>/<id>
-					return PathType.REPO_PATH;
-				}else (relative_path_components.length > 1) {
-					return PathType.USER_PATH;
-				}
-			}
+    // Check if is valid project
+    if ( relative_path_components[0] === "project" ) {
+      if (relative_path_components.length === 1) {
+        // REPO_PATH , PROJECT_PATH is reserved to project/<project_name>/<id>
+        return PathType.REPO_PATH;
+      }else (relative_path_components.length > 1) {
+        return PathType.PROJECT_PATH;
+      }
+    } else if( relative_path_components[0] === "user" ) {
+      // Check if valid user
+      if (relative_path_components.length === 1) {
+        // REPO_PATH , PROJECT_PATH is reserved to project/<project_name>/<id>
+        return PathType.REPO_PATH;
+      }else (relative_path_components.length > 1) {
+        return PathType.USER_PATH;
+      }
+    }
 
-			return PathType.UNKNOWN;
-		}
+    return PathType.UNKNOWN;
+  }
 
 }
 
 
-module.exports = Repo;
+module.exports = { Repo, PathType };
