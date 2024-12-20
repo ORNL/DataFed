a4e4fd855d75c4901deaa15e8a181572b59a4a27
Anthony Ramirez <ramirezat@ornl.gov>
2024-12-20 11:59:30 -0500

    Fix naming of user_router test so it will be picked up with sane specification. Add test to CMakeLists

--- core/database/foxx/tests/user_router.test.js
+++ core/database/foxx/tests/user_router.test.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 const assert = chai.assert;
 const user_router = require("../api/user_router");
 
-describe("token/set", () => {
+describe("user_router: the Foxx microservice user_router token/set endpoint", () => {
     it("should not pass this test test", () => {
         expect(false).to.be.true;
     });
