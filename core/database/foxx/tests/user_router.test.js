4c7a43d3f8458fe0edf5c28ddfcd7eef1a382e9b
Anthony Ramirez <ramirezat@ornl.gov>
2024-12-20 16:23:38 -0500

    Remove intentionally failing test. Break now working test into logical sections. Update comments.

--- core/database/foxx/tests/user_router.test.js
+++ core/database/foxx/tests/user_router.test.js
@@ -1,4 +1,5 @@
 "use strict"
+// NOTE: completion of tests requires successful run of user_fixture.js script
 
 // Integration test of API
 const { expect } = require("chai");
@@ -6,23 +7,14 @@ const request = require("@arangodb/request");
 const { baseUrl } = module.context;
 const { db } = require("@arangodb");
 
-// this will not be needed for the required integration test
-const user_router = require("../api/user_router");
-
 const usr_base_url = `${baseUrl}/usr`
 
 
 // NOTE: describe block strings are compared against test specification during test call, not file name
 describe("user_router: the Foxx microservice user_router token/set endpoint", () => {
-    it("should not pass this test test", () => {
-        expect(false).to.be.true;
-    });
-
-    // TODO: in order for this test to function we need to first at least create a user
-    //  to achieve this I am going to user a script to create a user fixture, this will be in the test dir
     it("should accept a valid user's token and execute an update", () => {
+        // arrange
         // NOTE: the get request has query params instead of a body
-
         const query_params = {
             _key: "testUser0",
             access: "asdf",
@@ -30,7 +22,11 @@ describe("user_router: the Foxx microservice user_router token/set endpoint", ()
         };
         // TODO: make encoded query params less hard coded
         const request_string = `${usr_base_url}/token/set?client=${query_params._key}&access=${query_params.access}&refresh=${query_params.refresh}&expires_in=500000`
+
+        // act
         const response = request.get(request_string);
+
+        // assert
         expect(response.status).to.equal(204);  // should pass
 
         const user_token_data = db.u.document({_key: query_params._key});
