fca275131cd9f223bb061927d3c4607327b953f6
Anthony Ramirez <ramirezat@ornl.gov>
2024-12-20 10:48:02 -0500

    Stub testing for user_router

---
+++ core/database/foxx/tests/user_router.test.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// Integration test of API
+const chai = require("chai");
+const should = chai.should();
+const expect = chai.expect;
+const assert = chai.assert;
+const user_router = require("../api/user_router");
+
+describe("token/set", () => {
+    it("should not pass this test test", () => {
+        expect(false).to.be.true;
+    });
+
+});
