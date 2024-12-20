dcd0e645951aa4dc26a6e1041bed67ffa18f484b
Anthony Ramirez <ramirezat@ornl.gov>
2024-12-20 15:19:12 -0500

    Fix manifest json. Hard code user collection check. Set time variable.

--- core/database/foxx/tests/user_fixture.js
+++ core/database/foxx/tests/user_fixture.js
@@ -1,11 +1,11 @@
 const { db } = require("@arangodb");
 const g_lib = require("../api/support");
-const users = module.context.collectionName("u");
 
 const uid = "testUser0";
+const current_time = Math.floor(Date.now() / 1000)
 
-if (!db._collection(users)) {
-    throw "This collection does not exist"
+if (!db._collection("u")) {
+    throw "This collection - u - does not exist"
 } else if (!db.u.exists(uid)) {
     const user_data = {
         _key: uid,
@@ -16,8 +16,8 @@ if (!db._collection(users)) {
         max_coll: g_lib.DEF_MAX_COLL,
         max_proj: g_lib.DEF_MAX_PROJ,
         max_sav_qry: g_lib.DEF_MAX_SAV_QRY,
-        ct: time,
-        ut: time
+        ct: current_time,
+        ut: current_time
     };
 
     db.u.save(user_data);
