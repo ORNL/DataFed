bc11aaaad26e9779cc7e48eb83c8f020b00e068d
Anthony Ramirez <ramirezat@ornl.gov>
2024-12-20 16:14:30 -0500

    Use correct path for router to user endpoints. Extract some query params for validation.

--- core/database/foxx/tests/user_router.test.js
+++ core/database/foxx/tests/user_router.test.js
@@ -4,9 +4,12 @@
 const { expect } = require("chai");
 const request = require("@arangodb/request");
 const { baseUrl } = module.context;
+const { db } = require("@arangodb");
 
 // this will not be needed for the required integration test
 const user_router = require("../api/user_router");
+
+const usr_base_url = `${baseUrl}/usr`
 
 
 // NOTE: describe block strings are compared against test specification during test call, not file name
@@ -19,8 +22,18 @@ describe("user_router: the Foxx microservice user_router token/set endpoint", ()
     //  to achieve this I am going to user a script to create a user fixture, this will be in the test dir
     it("should accept a valid user's token and execute an update", () => {
         // NOTE: the get request has query params instead of a body
+
+        const query_params = {
+            _key: "testUser0",
+            access: "asdf",
+            refresh: "jkl",
+        };
         // TODO: make encoded query params less hard coded
-        const request_string = `${baseUrl}/token/set?client=testUser0&access=asdf&refresh=jkl&expires_in=500000`
+        const request_string = `${usr_base_url}/token/set?client=${query_params._key}&access=${query_params.access}&refresh=${query_params.refresh}&expires_in=500000`
         const response = request.get(request_string);
+        expect(response.status).to.equal(204);  // should pass
+
+        const user_token_data = db.u.document({_key: query_params._key});
+        expect(user_token_data).to.include(query_params);
     });
 });
