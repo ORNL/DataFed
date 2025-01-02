08d7be99be6c20b75ce439a9a01b01a589a59476
JoshuaSBrown <brownjs@ornl.gov>
2025-01-02 09:54:15 -0500

    Turn browser off

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  env: {
+    browser: false,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended'],
+  rules: {
+    'no-console': 'warn',
+  },
+});
+
