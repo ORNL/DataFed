88e42b88d2170caa12a805aa0993d850246e37ee
JoshuaSBrown <brownjs@ornl.gov>
2025-05-01 10:27:50 -0400

    Add cipher engine test with libjson value

--- common/tests/unit/test_CipherEngine.cpp
+++ common/tests/unit/test_CipherEngine.cpp
@@ -7,6 +7,8 @@
 #include "../../include/common/CipherEngine.hpp"
 #include "../../include/common/Util.hpp"
 
+#include "common/libjson.hpp"
+
 //Standard includes
 #include <string.h>
 #include <iostream>
@@ -29,7 +31,6 @@ BOOST_AUTO_TEST_CASE(test_EncryptionDecryption)
     CipherEngine testCipher(key);
 
     //Sets struct CipherString: which contains cipherText, cipherIV, cipherPaddedLen
-    unsigned char iv[16];
 
     string msg = "AgdzegjlPyyoDa56Bx5yobvJYEjdGr2YpGYJybE7x4Bq42pQ3zuXCb8YQyn0EqEB7vjPx3GlNlKwkEsMnNokqfxq926";
     //string msg = "Hello World";
@@ -54,6 +55,69 @@ BOOST_AUTO_TEST_CASE(test_EncryptionDecryption)
     //START OF ENCRYPTION
     std::string unencrypted_msg;
     unencrypted_msg = testCipher.decrypt(returnObj2); 
+    std::cout << "Unencrypted Message:" << unencrypted_msg << std::endl;
+    BOOST_CHECK(msg.compare(unencrypted_msg) == 0);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_EncryptionDecryptionJSONValue)
+{
+
+    std::cout << "THIS IS A NEW FLAG" << std::endl;
+    unsigned char key[32];
+    //CipherEngine::generateEncryptionKey(key);
+    readFile("../../build/core/server/datafed-token-key.txt", 32, key);
+    
+    //Construct
+    CipherEngine testCipher(key);
+
+    //Sets struct CipherString: which contains cipherText, cipherIV, cipherPaddedLen
+
+    string msg = "AgdzegjlPyyoDa56Bx5yobvJYEjdGr2YpGYJybE7x4Bq42pQ3zuXCb8YQyn0EqEB7vjPx3GlNlKwkEsMnNokqfxq926";
+    //string msg = "Hello World";
+    //CipherEngine::generateIV(iv);
+
+    //Here if parties would like to use their own IV GENERATOR
+    //CipherEngine::CipherString returnObj = testCipher.encrypt(iv, msg);
+    
+    CipherEngine::CipherString returnObj2 = testCipher.encrypt(msg);
+ 
+/*
+    std::cout << "Rough Test1:" << encrypted_access_string << std::endl; 
+    std::string testString;
+
+    std::memcpy(returnObj2.encrypted_msg, testString.c_str(), returnObj2.encrypted_msg_len);
+    std::cout << "Rough Test2:" << testString << std::endl;    
+*/
+    std::cout << "Encrypted Message:\n" << returnObj2.encrypted_msg << std::endl;
+    std::cout << "IV:\n" << returnObj2.iv << std::endl;    
+    std::cout << "Encrypted Message Len:\n" << returnObj2.encrypted_msg_len << std::endl;
+
+    libjson::Value value;
+    std::string json_document = std::string("{ \"access\": \"") + returnObj2.encrypted_msg + "\", \"access_len\": 96, \"access_iv\": \"" + returnObj2.iv + "\"}";
+
+    value.fromString(json_document);
+    libjson::Value::Object &obj = value.asObject();
+    //START OF ENCRYPTION
+    //
+    //
+    CipherEngine testCipher2(key);
+    CipherEngine::CipherString encoded_access_obj = testCipher2.createCipherString();
+    encoded_access_obj.encrypted_msg_len = obj.getNumber("access_len");
+    encoded_access_obj.encrypted_msg = new char[129]; // add 1 for null terminator
+
+    std::string access = obj.getString("access");
+    memcpy(encoded_access_obj.encrypted_msg, access.c_str(), 128);
+    encoded_access_obj.encrypted_msg[128] = '\0'; // null terminate
+  
+  // Do the same for IV
+    std::string access_iv = obj.getString("access_iv");
+    encoded_access_obj.iv = new char[25];
+    memcpy(encoded_access_obj.iv, access_iv.c_str(), 24);
+    encoded_access_obj.iv[24] = '\0';
+
+    std::string unencrypted_msg;
+    unencrypted_msg = testCipher.decrypt(encoded_access_obj); 
     std::cout << "Unencrypted Message:" << unencrypted_msg << std::endl;
     BOOST_CHECK(msg.compare(unencrypted_msg) == 0);
 }
