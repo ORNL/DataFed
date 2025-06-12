af901b1ad628c0abc58eabda7b74169b041a5d71
Austin Hampton <amh107@latech.edu>
2025-06-12 10:22:59 -0400

    Removed unnecessary function handleError

--- common/include/common/Util.hpp
+++ common/include/common/Util.hpp
@@ -24,7 +24,6 @@ void generateKeys(std::string &a_pub_key, std::string &a_priv_key);
 void generateZMQKeys(std::string &a_pub_key, std::string &a_priv_key);
 void hexDump(const char *a_buffer, const char *a_buffer_end,
              std::ostream &a_out);
-void handleErrors(void);
 std::string escapeCSV(const std::string &a_value);
 std::string escapeJSON(const std::string &a_value);
 bool to_uint32(const char *a_str, uint32_t &a_out);
