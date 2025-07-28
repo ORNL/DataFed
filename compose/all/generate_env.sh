198aa77aef392c9d4f5e6c2b91d2e19ac10fd37f
Blake Nedved <nedvedba@ornl.gov>
2025-07-28 06:19:26 -0400

    reworked how environment file is read and certs are generated for #1529

--- compose/all/generate_env.sh
+++ compose/all/generate_env.sh
@@ -6,5 +6,5 @@ PROJECT_ROOT=$(realpath "${SOURCE}/../../")
 # Variables specific to running the compose instance
 export DATAFED_COMPOSE_REPO_DOMAIN="datafed-repo"
 
-"${PROJECT_ROOT}/scripts/compose_generate_env.sh" -d "$(pwd)"
+"${PROJECT_ROOT}/scripts/compose_generate_env.sh" -d "$(pwd)" $@
 
