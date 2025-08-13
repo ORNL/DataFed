f3ce4564db0f8400079092ba8a3c7f8be7ed50fd
Austin Hampton <ahampton4836@gmail.com>
2025-08-13 09:56:35 -0400

    style: Formatted globus scripts

--- scripts/globus/setup_globus.sh
+++ scripts/globus/setup_globus.sh
@@ -11,62 +11,56 @@ SOURCE=$(dirname "$SCRIPT")
 PROJECT_ROOT=$(realpath ${SOURCE}/../../)
 source ${PROJECT_ROOT}/config/datafed.sh
 
-if [ -z "$DATAFED_GCS_ROOT_NAME" ]
-then
+if [ -z "$DATAFED_GCS_ROOT_NAME" ]; then
   echo "DATAFED_GCS_ROOT_NAME is not defined cannot run $SCRIPT"
   exit 1
 fi
 
-if [ -z "$DATAFED_GCS_COLLECTION_ROOT_PATH" ]
-then
+if [ -z "$DATAFED_GCS_COLLECTION_ROOT_PATH" ]; then
   echo "DATAFED_GCS_COLLECTION_ROOT_PATH is not defined cannot run $SCRIPT"
   exit 1
 fi
 
-if [ -z "$DATAFED_REPO_ID_AND_DIR" ]
-then
+if [ -z "$DATAFED_REPO_ID_AND_DIR" ]; then
   echo "DATAFED_REPO_ID_AND_DIR is not defined cannot run $SCRIPT"
   exit 1
 fi
 
-if [ -z "$DATAFED_GLOBUS_CRED_FILE_PATH" ]
-then
+if [ -z "$DATAFED_GLOBUS_CRED_FILE_PATH" ]; then
   echo "DATAFED_GLOBUS_CRED_FILE_PATH is not defined cannot run $DATAFED_GLOBUS_CRED_FILE_PATH"
   exit 1
 else
   CRED_FILE_PATH="$DATAFED_GLOBUS_CRED_FILE_PATH"
 fi
 
-if [ -z "${DATAFED_GLOBUS_SUBSCRIPTION}" ]
-then
+if [ -z "${DATAFED_GLOBUS_SUBSCRIPTION}" ]; then
   echo "DATAFED_GLOBUS_SUBSCRIPTION not defined"
   DATAFED_GLOBUS_SUBSCRIPTION=""
 else
   DATAFED_GLOBUS_SUBSCRIPTION=$(printenv DATAFED_GLOBUS_SUBSCRIPTION)
 fi
 
-if [ -z "$DATAFED_GLOBUS_ALLOWED_DOMAINS" ]
-then
+if [ -z "$DATAFED_GLOBUS_ALLOWED_DOMAINS" ]; then
   echo "DATAFED_GLOBUS_ALLOWED_DOMAINS is not defined shoudl be i.e. globusid.org or gmail.com or ornl.gov or cu.edu or something of the sort."
   exit 1
 fi
 
 if [ -f "$CRED_FILE_PATH" ]; then
-    echo "File exists! $CRED_FILE_PATH"
+  echo "File exists! $CRED_FILE_PATH"
 else
-    echo "File does not exist. $CRED_FILE_PATH"
-		echo "run the Globus python script first"
-		exit 1
+  echo "File does not exist. $CRED_FILE_PATH"
+  echo "run the Globus python script first"
+  exit 1
 fi
 
 GATEWAY_NAME="${DATAFED_GCS_ROOT_NAME} Storage Gateway"
 COLLECTION_NAME="${DATAFED_GCS_ROOT_NAME} Collection Mapped"
 GUEST_COLLECTION_NAME="${DATAFED_GCS_ROOT_NAME} Collection Guest"
 
-gateway_line=$( globus-connect-server storage-gateway list | grep "$GATEWAY_NAME" || echo "")
+gateway_line=$(globus-connect-server storage-gateway list | grep "$GATEWAY_NAME" || echo "")
 
 echo "$DATAFED_GLOBUS_REPO_USER"
-cat << EOF > mapping.json 
+cat <<EOF >mapping.json
 {
   "DATA_TYPE": "expression_identity_mapping#1.0.0",
   "mappings": [
@@ -85,25 +79,24 @@ EOF
 # from the domain
 DOMAINS="--domain $DATAFED_GLOBUS_ALLOWED_DOMAINS --domain clients.auth.globus.org"
 
-echo "{" > path_restriction.json
-echo "  \"DATA_TYPE\": \"path_restrictions#1.0.0\"," >> path_restriction.json
-echo "  \"read_write\": [\"${DATAFED_GCS_COLLECTION_ROOT_PATH}/${DATAFED_REPO_ID_AND_DIR}\"]" >> path_restriction.json
-echo "}" >> path_restriction.json
+echo "{" >path_restriction.json
+echo "  \"DATA_TYPE\": \"path_restrictions#1.0.0\"," >>path_restriction.json
+echo "  \"read_write\": [\"${DATAFED_GCS_COLLECTION_ROOT_PATH}/${DATAFED_REPO_ID_AND_DIR}\"]" >>path_restriction.json
+echo "}" >>path_restriction.json
 
-if [ -z "$gateway_line" ]
-then
-# Check if it already exists
+if [ -z "$gateway_line" ]; then
+  # Check if it already exists
   globus-connect-server storage-gateway create posix \
     "$GATEWAY_NAME" \
-    ${DOMAINS}  \
+    ${DOMAINS} \
     --restrict-paths file:path_restriction.json \
     --identity-mapping file:mapping.json
 
 else
 
   spaces_in_name=$(echo $GATEWAY_NAME | awk '{print gsub("[ \t]",""); exit}')
-  columns=$(( $spaces_in_name + 3 ))
-  uuid_of_storage_gateway=$( globus-connect-server storage-gateway list | grep "$GATEWAY_NAME" | awk -v col=$columns '{ print $col }')
+  columns=$(($spaces_in_name + 3))
+  uuid_of_storage_gateway=$(globus-connect-server storage-gateway list | grep "$GATEWAY_NAME" | awk -v col=$columns '{ print $col }')
 
   globus-connect-server storage-gateway update posix \
     "$uuid_of_storage_gateway" \
@@ -117,22 +110,20 @@ fi
 mkdir -p "${DATAFED_GCS_COLLECTION_ROOT_PATH}/${DATAFED_REPO_ID_AND_DIR}/user"
 mkdir -p "${DATAFED_GCS_COLLECTION_ROOT_PATH}/${DATAFED_REPO_ID_AND_DIR}/project"
 
-collection_line=$( globus-connect-server collection list | grep "$COLLECTION_NAME" || echo "")
+collection_line=$(globus-connect-server collection list | grep "$COLLECTION_NAME" || echo "")
 
 spaces_in_name=$(echo $GATEWAY_NAME | awk '{print gsub("[ \t]",""); exit}')
-columns=$(( $spaces_in_name + 3 ))
-uuid_of_storage_gateway=$( globus-connect-server storage-gateway list | grep "$GATEWAY_NAME" | awk -v col=$columns '{ print $col }')
+columns=$(($spaces_in_name + 3))
+uuid_of_storage_gateway=$(globus-connect-server storage-gateway list | grep "$GATEWAY_NAME" | awk -v col=$columns '{ print $col }')
 
 # If not empty
 extra_collection_arg=""
-if [ -n "${DATAFED_GLOBUS_SUBSCRIPTION}" ]
-then
+if [ -n "${DATAFED_GLOBUS_SUBSCRIPTION}" ]; then
   globus-connect-server endpoint set-subscription-id "${DATAFED_GLOBUS_SUBSCRIPTION}"
   extra_collection_arg="--allow-guest-collections"
 fi
 
-if [ -z "$collection_line" ]
-then
+if [ -z "$collection_line" ]; then
 
   # NOTE enable-anonymous-writes is allowed without a subscription
   # NOTE allow-guest-collections requires a subscription
@@ -145,7 +136,7 @@ then
     --disable-https "$extra_collection_arg"
 else
 
-  uuid_of_collection=$( globus-connect-server collection list | grep "$COLLECTION_NAME" | awk '{ print $1 }')
+  uuid_of_collection=$(globus-connect-server collection list | grep "$COLLECTION_NAME" | awk '{ print $1 }')
   # NOTE enable-anonymous-writes is allowed without a subscription
   # NOTE allow-guest-collections requires a subscription
   globus-connect-server collection update \
