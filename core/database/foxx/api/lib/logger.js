b86bc22947d722df3cd5f977ddae2d5fdd8b7147
Austin Hampton <ahampton4836@gmail.com>
2025-09-16 15:55:26 +0000

    chore: Auto-format JavaScript files with Prettier

--- core/database/foxx/api/lib/logger.js
+++ core/database/foxx/api/lib/logger.js
@@ -1,83 +1,93 @@
 "use strict";
 
-
 function logRequestSuccess({
-  client,
-  correlationId,
-  httpVerb,
-  routePath,
-  status,
-  description,
-  extra
+    client,
+    correlationId,
+    httpVerb,
+    routePath,
+    status,
+    description,
+    extra,
 }) {
-  // helper to pad fields
-  const pad = (label, value, length = 20) =>
-    `${label}: ${value || "unknown"}`.padEnd(length, " ");
-  
-  console.info(
-    pad("Client", client) + " | " +
-    pad("Correlation_ID", correlationId) + " | " +
-    pad("HTTP", httpVerb) + " | " +
-    pad("Route", routePath) + " | " +
-    pad("Status", status) + " | " +
-    pad("Desc", description) + "|" +
-    pad("Extra", typeof extra === 'object' ? JSON.stringify(extra) : extra)
-  );
+    // helper to pad fields
+    const pad = (label, value, length = 20) =>
+        `${label}: ${value || "unknown"}`.padEnd(length, " ");
+
+    console.info(
+        pad("Client", client) +
+            " | " +
+            pad("Correlation_ID", correlationId) +
+            " | " +
+            pad("HTTP", httpVerb) +
+            " | " +
+            pad("Route", routePath) +
+            " | " +
+            pad("Status", status) +
+            " | " +
+            pad("Desc", description) +
+            "|" +
+            pad("Extra", typeof extra === "object" ? JSON.stringify(extra) : extra),
+    );
 }
 
 function logRequestFailure({
-  client,
-  correlationId,
-  httpVerb,
-  routePath,
-  status,
-  description,
-  extra,
-  message,
-  stack
+    client,
+    correlationId,
+    httpVerb,
+    routePath,
+    status,
+    description,
+    extra,
+    message,
+    stack,
 }) {
-  // helper to pad fields
-  const pad = (label, value, length = 20) =>
-    `${label}: ${value || "unknown"}`.padEnd(length, " ");
-  console.error(
-    pad("Client:", client) + " | " +
-    pad("Correlation_ID:", correlationId) + " | " +
-    pad("HTTP:", httpVerb) + " | " +
-    pad("Route:", routePath) + " | " +
-    pad("Status:", status) + " | " +
-    pad("Desc:", description) + "|" +
-    pad("Extra", typeof extra === 'object' ? JSON.stringify(extra) : extra) + "|" +
-    pad("Err:", message) + "|" +
-    pad("Stack:", stack)
-  );
+    // helper to pad fields
+    const pad = (label, value, length = 20) =>
+        `${label}: ${value || "unknown"}`.padEnd(length, " ");
+    console.error(
+        pad("Client:", client) +
+            " | " +
+            pad("Correlation_ID:", correlationId) +
+            " | " +
+            pad("HTTP:", httpVerb) +
+            " | " +
+            pad("Route:", routePath) +
+            " | " +
+            pad("Status:", status) +
+            " | " +
+            pad("Desc:", description) +
+            "|" +
+            pad("Extra", typeof extra === "object" ? JSON.stringify(extra) : extra) +
+            "|" +
+            pad("Err:", message) +
+            "|" +
+            pad("Stack:", stack),
+    );
 }
 
+function logRequestStarted({ client, correlationId, httpVerb, routePath, status, description }) {
+    // helper to pad fields
+    const pad = (label, value, length = 20) =>
+        `${label}: ${value || "unknown"}`.padEnd(length, " ");
 
-function logRequestStarted({
-  client,
-  correlationId,
-  httpVerb,
-  routePath,
-  status,
-  description
-}) {
-  // helper to pad fields
-  const pad = (label, value, length = 20) =>
-    `${label}: ${value || "unknown"}`.padEnd(length, " ");
-
-  console.info(
-    pad("Client:", client) + " | " +
-    pad("Correlation_ID:", correlationId) + " | " +
-    pad("HTTP:", httpVerb) + " | " +
-    pad("Route:", routePath) + " | " +
-    pad("Status:", status) + " | " +
-    pad("Desc:", description)
-  );
+    console.info(
+        pad("Client:", client) +
+            " | " +
+            pad("Correlation_ID:", correlationId) +
+            " | " +
+            pad("HTTP:", httpVerb) +
+            " | " +
+            pad("Route:", routePath) +
+            " | " +
+            pad("Status:", status) +
+            " | " +
+            pad("Desc:", description),
+    );
 }
 
 // Export the functions
 module.exports = {
-  logRequestSuccess,
-  logRequestFailure,
-  logRequestStarted
+    logRequestSuccess,
+    logRequestFailure,
+    logRequestStarted,
 };
