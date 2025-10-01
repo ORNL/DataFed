e2ab579cb95f43c123384f8918abd843915de863
Austin Hampton <ahampton4836@gmail.com>
2025-10-01 13:37:24 -0400

    feature: Implemented logging functions, first pass

---
+++ core/database/foxx/api/lib/logger.js
@@ -0,0 +1,115 @@
+"use strict";
+const g_lib=require("../support")
+
+function logRequestSuccess({
+    client,
+    correlationId,
+    httpVerb,
+    routePath,
+    status,
+    description,
+    extra,
+}) {
+    // helper to pad fields
+    const pad = (label, value, length = 20) =>
+        `${label}: ${value || "unknown"}`.padEnd(length, " ");
+
+    console.info(
+        pad("Client", client) +
+            " | " +
+            pad("Correlation_ID", correlationId) +
+            " | " +
+            pad("HTTP", httpVerb) +
+            " | " +
+            pad("Route", routePath) +
+            " | " +
+            pad("Status", status) +
+            " | " +
+            pad("Desc", description) +
+            " | " +
+            pad("Extra", typeof extra === "object" ? JSON.stringify(extra) : extra),
+    );
+}
+
+function logRequestFailure({
+    client,
+    correlationId,
+    httpVerb,
+    routePath,
+    status,
+    description,
+    extra,
+    error
+}) {
+    // helper to pad fields
+    const pad = (label, value, length = 20) =>
+        `${label}: ${value || "unknown"}`.padEnd(length, " ");
+    //PUT IF STATEMENT
+    if(g_lib.isInteger(error) || g_lib.isArray(error)){
+        console.error(
+            pad("Client", client) +
+            " | " +
+            pad("Correlation_ID", correlationId) +
+            " | " +
+            pad("HTTP", httpVerb) +
+            " | " +
+            pad("Route", routePath) +
+            " | " +
+            pad("Status", status) +
+            " | " +
+            pad("Desc", description) +
+            " | " +
+            pad("Extra", typeof extra === "object" ? JSON.stringify(extra) : extra) +
+            " | " +
+            pad("Error", error) +
+        );
+    }
+    else{
+        console.error(
+            pad("Client", client) +
+            " | " +
+            pad("Correlation_ID", correlationId) +
+            " | " +
+            pad("HTTP", httpVerb) +
+            " | " +
+            pad("Route", routePath) +
+            " | " +
+            pad("Status", status) +
+            " | " +
+            pad("Desc", description) +
+            " | " +
+            pad("Extra", typeof extra === "object" ? JSON.stringify(extra) : extra) +
+            " | " +
+            pad("Error", error.message) +
+            " | " +
+            pad("Stack", error.stack)
+        );
+    }
+}
+
+function logRequestStarted({ client, correlationId, httpVerb, routePath, status, description }) {
+    // helper to pad fields
+    const pad = (label, value, length = 20) =>
+        `${label}: ${value || "unknown"}`.padEnd(length, " ");
+
+    console.info(
+        pad("Client", client) +
+            " | " +
+            pad("Correlation_ID", correlationId) +
+            " | " +
+            pad("HTTP", httpVerb) +
+            " | " +
+            pad("Route", routePath) +
+            " | " +
+            pad("Status", status) +
+            " | " +
+            pad("Desc", description),
+    );
+}
+
+// Export the functions
+module.exports = {
+    logRequestSuccess,
+    logRequestFailure,
+    logRequestStarted,
+};
