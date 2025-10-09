0853d9bef3bb39dfcdc95c9158c871e12db0db91
JoshuaSBrown <brownjs@ornl.gov>
2025-10-09 13:55:09 -0400

    refactor: make timeouts more robust and configurable.

--- tests/end-to-end/web-UI/playwright.config.js
+++ tests/end-to-end/web-UI/playwright.config.js
@@ -15,13 +15,13 @@ module.exports = defineConfig({
     /* Run tests in files in parallel */
     fullyParallel: false,
 
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
+    /* Fail the build on CI_DATAFED_END_TO_END_WEB_RETRIES if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI_DATAFED_END_TO_END_WEB_RETRIES,
 
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    /* Retry on CI_DATAFED_END_TO_END_WEB_RETRIES only */
+    retries: process.env.CI_DATAFED_END_TO_END_WEB_RETRIES ? 2 : 0,
 
-    /* Opt out of parallel tests on CI. */
+    /* Opt out of parallel tests on CI_DATAFED_END_TO_END_WEB_RETRIES. */
     workers: 1,
 
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -31,9 +31,11 @@ module.exports = defineConfig({
     use: {
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
-
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
         ignoreHTTPSErrors: true,
-
+        actionTimeout: 45000,
+        navigationTimeout: 60000,
         launchOptions: {
             args: ["--ignore-certificate-errors"],
         },
