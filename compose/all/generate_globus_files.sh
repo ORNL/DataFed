f2a87bdd6870d60569eb553ad7126f04b45b471a
infispiel <infinite.loopholes@gmail.com>
2025-10-21 14:31:12 -0400

    Compose globus scripts now exit on bash error

--- compose/all/generate_globus_files.sh
+++ compose/all/generate_globus_files.sh
@@ -3,4 +3,6 @@ SCRIPT=$(realpath "${BASH_SOURCE[0]}")
 SOURCE=$(dirname "$SCRIPT")
 PROJECT_ROOT=$(realpath "${SOURCE}/../../")
 
+set -euf -o pipefail
+
 "${PROJECT_ROOT}/scripts/compose_generate_globus_files.sh" -d "$(pwd)"
--- compose/metadata/generate_globus_files.sh
+++ compose/metadata/generate_globus_files.sh
@@ -3,4 +3,6 @@ SCRIPT=$(realpath "${BASH_SOURCE[0]}")
 SOURCE=$(dirname "$SCRIPT")
 PROJECT_ROOT=$(realpath "${SOURCE}/../../")
 
+set -euf -o pipefail
+
 "${PROJECT_ROOT}/scripts/compose_generate_web_server_globus_credentials.sh" -d "$(pwd)"
