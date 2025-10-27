963db8a8b71a72f81330bba97aca6c59d2128ee7
Austin Hampton <ahampton4836@gmail.com>
2025-10-27 09:40:45 -0400

    refactor: added cleanup after tests

--- core/database/foxx/tests/query_router.test.js
+++ core/database/foxx/tests/query_router.test.js
@@ -13,6 +13,14 @@ const { db } = require("@arangodb");
 const qry_base_url = `${baseUrl}/qry`;
 
 describe("unit_query_router: the Foxx microservice qry_router endpoints", () => {
+    after(function () {
+        const collections = ["u", "task"];
+        collections.forEach((name) => {
+            let col = db._collection(name);
+            if (col) col.truncate();
+        });
+    });
+
     beforeEach(() => {
         const collections = ["u", "qry"];
         collections.forEach((name) => {
