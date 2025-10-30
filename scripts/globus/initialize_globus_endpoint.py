5ab04249b12fbc6dc4ecc0a7e79567e6e965de47
Joshua S Brown <joshbro42867@yahoo.com>
2025-10-30 11:26:06 -0400

    [DAPS-1741] Update: native client id in intialize_globus_endpoint and globus_clea… (#1741)

--- scripts/globus/initialize_globus_endpoint.py
+++ scripts/globus/initialize_globus_endpoint.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 # Hard coded Native Client ID
-CLIENT_ID = "f8d0afca-7ac4-4a3c-ac05-f94f5d9afce8"
+CLIENT_ID = "d7a7db79-c746-4947-9273-54cf578e4785"
 
 # The Globus project the GCS endpoint will be created in
 default_DATAFED_GCS_ROOT_NAME = "DataFed Repo"
