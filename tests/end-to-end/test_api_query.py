2a0d0cecb52b1ffa07e83b54497179ada9f56716
JoshuaSBrown <brownjs@ornl.gov>
2025-12-01 17:06:05 -0500

    debug api_query test.

--- tests/end-to-end/test_api_query.py
+++ tests/end-to-end/test_api_query.py
@@ -168,8 +168,10 @@ class TestDataFedPythonAPIQueryCRUD(unittest.TestCase):
         print("Created record")
         print(record)
 
-    def test_query_create_delete(self):
+        print("Response from dataView")
+        print(self._df_api.dataView(record[0].data.id))
 
+    def test_query_create_delete(self):
         search_query = self._df_api.queryCreate(title="Search for Adamantium", owner="u/" + self._username, coll=["root"], meta="md.creator == 'Lex Luther'")
 
         print("Search query create")
