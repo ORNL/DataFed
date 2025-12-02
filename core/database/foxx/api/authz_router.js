39205595cac5539b72f0d0c97f70e62cd7b7601d
Austin Hampton <ahampton4836@gmail.com>
2025-12-02 14:40:33 +0000

    chore: Auto-format JavaScript files with Prettier

--- core/database/foxx/api/authz_router.js
+++ core/database/foxx/api/authz_router.js
@@ -204,7 +204,11 @@ router
                 httpVerb: "GET",
                 routePath: basePath + "/perm/check",
                 status: "Success",
-                description: "Checks client permissions for object. OBJ ID:" + req.queryParams.id + ", Permissions: " + req.queryParams.perms,
+                description:
+                    "Checks client permissions for object. OBJ ID:" +
+                    req.queryParams.id +
+                    ", Permissions: " +
+                    req.queryParams.perms,
                 extra: result,
             });
         } catch (e) {
@@ -214,7 +218,11 @@ router
                 httpVerb: "GET",
                 routePath: basePath + "/perm/check",
                 status: "Failure",
-                description: "Checks client permissions for object. OBJ ID:" + req.queryParams.id + ", Permissions: " + req.queryParams.perms,
+                description:
+                    "Checks client permissions for object. OBJ ID:" +
+                    req.queryParams.id +
+                    ", Permissions: " +
+                    req.queryParams.perms,
                 extra: result,
                 error: e,
             });
@@ -239,7 +247,8 @@ router
                 httpVerb: "GET",
                 routePath: basePath + "/perm/get",
                 status: "Started",
-                description: "Gets client permissions for object. Permissions:" + req.queryParams.perms,
+                description:
+                    "Gets client permissions for object. Permissions:" + req.queryParams.perms,
             });
 
             result = req.queryParams.perms ? req.queryParams.perms : permissions.PERM_ALL;
@@ -282,7 +291,8 @@ router
                 httpVerb: "GET",
                 routePath: basePath + "/perm/get",
                 status: "Success",
-                description: "Gets client permissions for object. Permissions:" + req.queryParams.perms,
+                description:
+                    "Gets client permissions for object. Permissions:" + req.queryParams.perms,
                 extra: result,
             });
         } catch (e) {
@@ -292,7 +302,8 @@ router
                 httpVerb: "GET",
                 routePath: basePath + "/perm/get",
                 status: "Failure",
-                description: "Gets client permissions for object. Permissions:" + req.queryParams.perms,
+                description:
+                    "Gets client permissions for object. Permissions:" + req.queryParams.perms,
                 extra: result,
                 error: e,
             });
