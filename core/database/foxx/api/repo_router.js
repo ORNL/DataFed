cdd65aff7aa48ebb3a8c67adf8426d4f28ebf91c
Austin Hampton <ahampton4836@gmail.com>
2026-01-08 19:15:03 +0000

    chore: Auto-format JavaScript files with Prettier

--- core/database/foxx/api/repo_router.js
+++ core/database/foxx/api/repo_router.js
@@ -157,11 +157,10 @@ router
                 routePath: basePath + "/view",
                 status: "Success",
                 description: `View repo server record: ${req.queryParams.id}`,
-                extra: 
-                {
+                extra: {
                     type: repo.type,
                     capacity: repo.capacity,
-                    admins: repo.admins
+                    admins: repo.admins,
                 },
             });
         } catch (e) {
@@ -171,11 +170,10 @@ router
                 routePath: basePath + "/view",
                 status: "Failure",
                 description: `View repo server record: ${req.queryParams.id}`,
-                extra: 
-                {
+                extra: {
                     type: repo.type,
                     capacity: repo.capacity,
-                    admins: repo.admins
+                    admins: repo.admins,
                 },
                 error: e,
             });
@@ -261,7 +259,7 @@ router
                 extra: {
                     type: repo_doc.type,
                     capacity: repo_doc.capacity,
-                    admins: repo_doc.admins
+                    admins: repo_doc.admins,
                 },
             });
         } catch (e) {
@@ -272,11 +270,10 @@ router
                 routePath: basePath + "/create",
                 status: "Failure",
                 description: `Create a server record: ${req.queryParams.id}`,
-                extra:                 
-                {
+                extra: {
                     type: repo_doc.type,
                     capacity: repo_doc.capacity,
-                    admins: repo_doc.admins
+                    admins: repo_doc.admins,
                 },
                 error: e,
             });
@@ -406,7 +403,7 @@ router
                 description: `Update a repo server record: ${req.queryParams.id}`,
                 extra: {
                     capacity: req.queryParam.capacity,
-                    admins: req.queryParams.admins
+                    admins: req.queryParams.admins,
                 },
             });
         } catch (e) {
@@ -419,7 +416,7 @@ router
                 description: `Update a repo server record: ${req.queryParams.id}`,
                 extra: {
                     capacity: req.queryParam.capacity,
-                    admins: req.queryParams.admins
+                    admins: req.queryParams.admins,
                 },
                 error: e,
             });
