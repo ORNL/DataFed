d51f01a3946ed4cbe4fe2f05a0cb21085d1a77f0
JoshuaSBrown <brownjs@ornl.gov>
2026-02-05 21:11:26 +0000

    chore: Auto-format JavaScript files with Prettier

--- web/datafed-ws.js
+++ web/datafed-ws.js
@@ -2091,7 +2091,9 @@ function sendMessage(a_msg_name, a_msg_data, a_req, a_resp, a_cb, a_anon) {
                 getCurrentLineNumber(),
                 "MsgType is: " +
                     msg_info.field_id +
-                    " (" + a_msg_name + ")" +
+                    " (" +
+                    a_msg_name +
+                    ")" +
                     " Writing ctx to frame, " +
                     ctx +
                     " buffer size " +
@@ -2113,7 +2115,9 @@ function sendMessage(a_msg_name, a_msg_data, a_req, a_resp, a_cb, a_anon) {
                 getCurrentLineNumber(),
                 "MsgType is: " +
                     msg_info.field_id +
-                    " (" + a_msg_name + ")" +
+                    " (" +
+                    a_msg_name +
+                    ")" +
                     " Writing ctx to frame, " +
                     ctx +
                     " buffer size " +
@@ -2161,7 +2165,9 @@ function sendMessageDirect(a_msg_name, a_client, a_msg_data, a_cb) {
                 getCurrentLineNumber(),
                 "MsgType is: " +
                     msg_info.field_id +
-                    " (" + a_msg_name + ")" +
+                    " (" +
+                    a_msg_name +
+                    ")" +
                     " Direct Writing ctx to frame, " +
                     ctx +
                     " buffer size " +
@@ -2183,7 +2189,9 @@ function sendMessageDirect(a_msg_name, a_client, a_msg_data, a_cb) {
                 getCurrentLineNumber(),
                 "MsgType is: " +
                     msg_info.field_id +
-                    " (" + a_msg_name + ")" +
+                    " (" +
+                    a_msg_name +
+                    ")" +
                     " Direct Writing ctx to frame, " +
                     ctx +
                     " buffer size " +
@@ -2226,9 +2234,9 @@ function processEnvelope(root) {
         }
 
         var entry = {
-            type: msgType,          // protobufjs Type for encode/decode
+            type: msgType, // protobufjs Type for encode/decode
             field_name: field.name, // envelope oneof field name
-            field_id: field.id,     // envelope field number = msg_type in frame
+            field_id: field.id, // envelope field number = msg_type in frame
         };
 
         g_msg_by_id[field.id] = entry;
@@ -2334,7 +2342,10 @@ g_core_sock.on(
                     logger.error(
                         "g_core_sock.on",
                         getCurrentLineNumber(),
-                        "ERROR: envelope decode failed: " + err + " correlation_id: " + correlation_id,
+                        "ERROR: envelope decode failed: " +
+                            err +
+                            " correlation_id: " +
+                            correlation_id,
                     );
                 }
             } else {
