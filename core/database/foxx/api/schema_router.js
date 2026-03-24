c615fc7f770de510efb6fdd9e84f60f331bb50b9
Joshua S Brown <joshbro42867@yahoo.com>
2026-03-24 07:33:53 -0400

    [DAPS-1857-2] python client schema support (#1895)
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- core/database/foxx/api/schema_router.js
+++ core/database/foxx/api/schema_router.js
@@ -58,7 +58,7 @@ function parseSchemaId(schId) {
     }
 
     if (colonCount === 0) {
-        return { id: schId, ver: 0 };
+        return { id: schId, ver: null };
     }
 
     const idx = schId.indexOf(":");
